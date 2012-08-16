ced0acdc2bf7f5ba8e62aaba2b1f3193fa1387ab
Hendy Irawan <hendy@soluvas.com>
2012-08-17 03:04:10 +0700

    Updates

--- stomp/src/main/java/org/soluvas/web/stomp/ProcessBackboneCollectionReset.java
+++ stomp/src/main/java/org/soluvas/web/stomp/ProcessBackboneCollectionReset.java
@@ -3,10 +3,11 @@ package org.soluvas.web.stomp;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.soluvas.async.CallbackAdapter;
 import org.soluvas.json.JsonUtils;
 import org.soluvas.process.ProcessRepository;
-import org.soluvas.web.nav.Menu;
 import org.soluvas.web.site.JavaScriptSourceImpl;
 
 /**
@@ -17,6 +18,7 @@ import org.soluvas.web.site.JavaScriptSourceImpl;
 @SuppressWarnings("serial")
 public class ProcessBackboneCollectionReset extends JavaScriptSourceImpl {
 
+	private transient Logger log = LoggerFactory.getLogger(ProcessBackboneCollectionReset.class);
 	private ProcessRepository processRepository;
 	
 	public ProcessBackboneCollectionReset(ProcessRepository processRepository, int weight) {
@@ -34,16 +36,20 @@ public class ProcessBackboneCollectionReset extends JavaScriptSourceImpl {
 	 */
 	@Override
 	public String getBody() {
-		final AtomicReference<String> body = new AtomicReference<String>();
-		processRepository.findAll(new CallbackAdapter<Void, Void, List<org.soluvas.process.Process>>() {
-			@Override
-			public void success(List<org.soluvas.process.Process> processes) {
-				String buf = "$(document).ready(function() {\n";
-				buf += "	processItems.reset( " + JsonUtils.asJson(processes) + " );\n";
-				buf += "});\n";
-				body.set( buf );
-			}
-		});
+		final AtomicReference<String> body = new AtomicReference<String>("");
+		try {
+			processRepository.findAll(new CallbackAdapter<Void, Void, List<org.soluvas.process.Process>>() {
+				@Override
+				public void success(List<org.soluvas.process.Process> processes) {
+					String buf = "$(document).ready(function() {\n";
+					buf += "	processItems.reset( " + JsonUtils.asJson(processes) + " );\n";
+					buf += "});\n";
+					body.set( buf );
+				}
+			});
+		} catch (Exception e) {
+			log.warn("Cannot get list of processes", e);
+		}
 		return body.get();
 	}
 
