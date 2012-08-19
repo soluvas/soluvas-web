fb3e0d634957519bc0f6b61f3a06dfa81e91824b
Hendy Irawan <hendy@soluvas.com>
2012-08-20 04:29:34 +0700

    EMF import package fix

--- backbone/src/main/resources/webresources/pref.locale.js
+++ backbone/src/main/resources/webresources/pref.locale.js
@@ -4,6 +4,22 @@ define(['sugar', 'sugar_id'],
 // 1. Locale defaults (TODO: should be global)
 Date.setLocale('id');
 
-return true;
+var PrefLocale = {
+	formatMoney: function(currencyCode, amount) {
+		var doubleAmount = typeof(amount) == 'string' ? amount.toNumber() : amount;
+		return currencyCode + ' ' + doubleAmount.format(0, '.', ',');
+	},
+	formatUnit: function(measurementUnit, amount) {
+		var unitStr = '';
+		switch (measurementUnit) {
+		case 'piece':
+			unitStr = amount >= 2 ? 'pcs' : 'pc';
+			break;
+		}
+		return amount + ' ' + unitStr;
+	}
+}
+
+return PrefLocale;
 
 });
