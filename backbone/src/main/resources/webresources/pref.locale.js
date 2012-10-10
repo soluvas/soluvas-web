068a1210b40f67581785079cbdbd940068d4d04d
Rudi Wijaya <rudi_mail90@yahoo.com>
2012-10-10 13:59:20 +0700

    Refs #723, #730, #731. Summary: 1. Bug fixing..

--- backbone/src/main/resources/webresources/pref.locale.js
+++ backbone/src/main/resources/webresources/pref.locale.js
@@ -3,6 +3,7 @@ define(['sugar', 'sugar_id'],
 
 // 1. Locale defaults (TODO: should be global)
 Date.setLocale('id');
+// TODO: set timezone
 
 var PrefLocale = {
 	formatMoney: function(currencyCode, amount) {
@@ -17,6 +18,17 @@ var PrefLocale = {
 			break;
 		}
 		return amount + ' ' + unitStr;
+	},
+	formatDate: function(date) {
+		if (date.minutesAgo() <= 1) {
+			return 'baru saja';
+		} else if (date.minutesAgo() < 60) {
+			return date.minutesAgo() + ' menit lalu';
+		} else if (date.hoursAgo() < 24) {
+			return date.hoursAgo() + ' jam lalu';
+		} else {
+			return date.long();
+		}
 	}
 }
 
