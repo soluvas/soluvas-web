2ebe2e023f3d7a4a0a126655bb6096c7d2ea0b8b
Hendy Irawan <hendy@soluvas.com>
2012-10-30 00:29:08 +0700

    NEW - task 928: Login / Authentication system using Apache Shiro http://mahameru.unfuddle.com/projects/1/tickets/by_number/928

--- login/src/main/java/org/soluvas/web/login/SecurityUtilsSubjectSupplier.java
+++ login/src/main/java/org/soluvas/web/login/SecurityUtilsSubjectSupplier.java
@@ -1,5 +1,6 @@
 package org.soluvas.web.login;
 
+import org.apache.felix.service.command.CommandSession;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 
@@ -8,6 +9,21 @@ import com.google.common.base.Supplier;
 /**
  * Provides {@link Subject} via {@link SecurityUtils#getSubject()}. Note that
  * this is not expected to work well in a multitenant OSGi environment.
+ * 
+ * The problem is, a "SecurityUtils.getSubject()" is definitely dependant on
+ * both the tenant and the subsystem :
+ * <ol>
+ * <li>Wicket subsystem: get tenant from Wicket's Application.get(), get session
+ * ID from HttpServletRequest Cookie</li>
+ * <li>JAX-RS subsystem: get tenant from analyzing the URI, get session ID from
+ * HttpServletRequest Cookie</li>
+ * <li>Shell subsystem: get tenant from {@link CommandSession}, get session ID
+ * also from {@link CommandSession}</li>
+ * </ol>
+ * 
+ * On all three scenarios, the session itself is stored elsewhere, by Shiro,
+ * probably in a MongoDB database, so sessions can be shared in a cluster of
+ * Karaf nodes.
  * 
  * @author ceefour
  */
