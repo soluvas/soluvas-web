578e5c3cc17faeca9a99911074086172817b238a
Hendy Irawan <hendy@soluvas.com>
2012-11-01 19:24:10 +0700

    Incomplete - task : Security: Add and remove roles

--- login/src/main/java/org/soluvas/web/login/SoluvasRealm.java
+++ login/src/main/java/org/soluvas/web/login/SoluvasRealm.java
@@ -2,15 +2,6 @@ package org.soluvas.web.login;
 
 import java.util.Set;
 
-import javax.annotation.Nullable;
-
-import org.apache.commons.pool.ObjectPool;
-import org.apache.directory.ldap.client.api.LdapConnection;
-import org.apache.directory.ldap.client.api.LdapConnectionConfig;
-import org.apache.directory.shared.ldap.model.entry.Entry;
-import org.apache.directory.shared.ldap.model.message.SearchScope;
-import org.apache.directory.shared.ldap.model.name.Dn;
-import org.apache.directory.shared.ldap.model.name.Rdn;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
@@ -23,12 +14,11 @@ import org.apache.shiro.realm.ldap.AbstractLdapRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.soluvas.ldap.LdapUtils;
 import org.soluvas.security.Permission;
 import org.soluvas.security.Role;
 import org.soluvas.security.SecurityCatalog;
+import org.soluvas.security.SecurityRepository;
 
-import com.google.common.base.Function;
 import com.google.common.base.Supplier;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
@@ -42,27 +32,10 @@ import com.google.common.collect.Sets;
  */
 public class SoluvasRealm extends AuthorizingRealm {
 
-	private transient Logger log = LoggerFactory.getLogger(SoluvasRealm.class);
+	transient Logger log = LoggerFactory.getLogger(SoluvasRealm.class);
 
-	/**
-	 * {@link LdapConnection} pool which full access used to read entries.
-	 */
-	private final ObjectPool<LdapConnection> ldapPool;
-	/**
-	 * (Context) Entry which acts as the root of the application.
-	 */
-	private final String domainBase;
-	/**
-	 * Entry which contains the <tt>person</tt> entries, relative to
-	 * <tt>domainBase</tt>.
-	 */
-	private final String usersRdn = "ou=users";
-	/**
-	 * Entry which contains the <tt>groupOfUniqueNames</tt> entries, relative to
-	 * <tt>domainBase</tt>. These will be mapped to Shiro roles.
-	 */
-	private final String groupsRdn = "ou=groups";
 	private final Supplier<SecurityCatalog> securityCatalogSupplier;
+	private final SecurityRepository securityRepo;
 
 	/**
 	 * 
@@ -71,59 +44,22 @@ public class SoluvasRealm extends AuthorizingRealm {
 	 * @param bindConfig
 	 *            LDAP configuration used to perform bind-based authentication.
 	 */
-	public SoluvasRealm(final ObjectPool<LdapConnection> ldapPool,
-			String domainBase, LdapConnectionConfig bindConfig,
-			Supplier<SecurityCatalog> securityCatalogSupplier) {
+	public SoluvasRealm(Supplier<SecurityCatalog> securityCatalogSupplier,
+			SecurityRepository securityRepo) {
 		super();
-		this.ldapPool = ldapPool;
-		this.domainBase = domainBase;
 		this.securityCatalogSupplier = securityCatalogSupplier;
-		setName(domainBase);
-		CredentialsMatcher cm = new LdapBindCredentialsMatcher(bindConfig,
-				usersRdn + "," + domainBase);
+		this.securityRepo = securityRepo;
+		setName(securityRepo.getDomainBase());
+		CredentialsMatcher cm = new LdapBindCredentialsMatcher(securityRepo.getBindConfig(),
+				securityRepo.getUsersRdn() + "," + securityRepo.getDomainBase());
 		setCredentialsMatcher(cm);
 	}
-
+	
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(
 			final PrincipalCollection principalCollection) {
 		final String userName = (String) principalCollection.getPrimaryPrincipal();
-		Set<String> ldapRoles = LdapUtils.withConnection(ldapPool,
-				new Function<LdapConnection, Set<String>>() {
-			@Override @Nullable
-					public Set<String> apply(@Nullable LdapConnection ldap) {
-						try {
-							final Dn userDn = new Dn(new Rdn("uid", userName),
-									new Dn(usersRdn, domainBase));
-							final Dn groupsDn = new Dn(groupsRdn, domainBase);
-							final String memberFilter = "(uniqueMember="
-									+ userDn.getName() + ")";
-							log.debug("Searching for {} in {}", memberFilter,
-									groupsDn.getName());
-							Set<String> roles = LdapUtils.transformSet(ldap
-									.search(groupsDn, memberFilter,
-											SearchScope.ONELEVEL),
-									new Function<Entry, String>() {
-										@Override
-										@Nullable
-										public String apply(
-												@Nullable Entry input) {
-											return input.getDn().getRdn()
-													.getValue().getString();
-										}
-									});
-							log.debug("User {} has {} roles: {}", userName,
-									roles.size(), roles);
-							return roles;
-						} catch (Exception e) {
-							log.error("Cannot get authorization info for "
-									+ userName, e);
-							throw new RuntimeException(
-									"Cannot get authorization info for "
-											+ userName, e);
-				}
-			}
-		});
+		final Set<String> ldapRoles = securityRepo.getPersonRoles(userName);
 
 		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
 		info.addRoles(ldapRoles);
@@ -167,17 +103,6 @@ public class SoluvasRealm extends AuthorizingRealm {
 						perm.toStringPermission(), userName, intersectingRoles);
 				info.addStringPermission(perm.toStringPermission());
 			}
-		}
-
-		if (info.getRoles().contains("admin")) {
-			info.addStringPermission("mall:*:*");
-			info.addStringPermission("shop:*:*");
-			info.addStringPermission("product:*:*");
-			info.addStringPermission("person:*:*");
-			info.addStringPermission("comment:*:*");
-			info.addStringPermission("story:*:*");
-			info.addStringPermission("like:*:*");
-			info.addStringPermission("salesorder:*:*");
 		}
 
 		// TODO: resource-level roles
