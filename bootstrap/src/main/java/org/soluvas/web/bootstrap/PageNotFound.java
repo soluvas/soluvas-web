2f7896546339e15e877bbbb5ceb363284b51ed7a
Atang Sutisna <atang@bippo.co.id>
2012-11-02 14:31:15 +0700

    add default page if page not available

---
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/PageNotFound.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.soluvas.web.bootstrap;
+
+
+
+/**
+ * @author atang
+ *
+ */
+@SuppressWarnings("serial")
+public class PageNotFound extends BootstrapPage {
+
+	public PageNotFound(){
+//		add(new Page404());
+	}
+}
