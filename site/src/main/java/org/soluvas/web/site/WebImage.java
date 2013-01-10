2aa6ca01fe388041f92ef5dbfa99c498af33824d
Ahmad Syarif Farsiado <farsiado@berbatik.com>
2013-01-10 19:33:59 +0700

    WebImage

---
+++ site/src/main/java/org/soluvas/web/site/WebImage.java
@@ -0,0 +1,67 @@
+package org.soluvas.web.site;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soluvas.commons.WebAddress;
+import org.soluvas.commons.inject.Supplied;
+
+import com.damnhandy.uri.template.UriTemplate;
+import com.damnhandy.uri.template.impl.ExpressionParseException;
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Image component that supports URI templating using {@link WebAddress}.
+ * @author rio
+ */
+@SuppressWarnings("serial")
+public class WebImage extends WebMarkupContainer {
+
+	private static final Logger log = LoggerFactory.getLogger(WebImage.class);
+	
+	@Inject @Supplied
+	private WebAddress webAddress;
+	
+	public WebImage(String id) {
+		super(id);
+	}
+	
+	@Override
+	protected void onComponentTag(ComponentTag tag) {
+		super.onComponentTag(tag);
+		final String srcTemplate = getMarkupAttributes().getString("src");
+		final Map<String, Object> vars = ImmutableMap.<String, Object>of(
+				"imagesUri", webAddress.getImagesUri(), "skinUri", webAddress.getSkinUri());
+		log.trace("Expanding {} using {}", getPageRelativePath(), srcTemplate);
+		try {
+			final String src = UriTemplate.fromTemplate(srcTemplate).expand(vars);
+			tag.put("src", src);
+		} catch (ExpressionParseException e) {
+			throw new SiteException(e, "WebImage %'s src invalid URI template or without variables, please use standard img for non-dynamic URIs: %s",
+					getPageRelativePath(), srcTemplate);
+		}
+	}
+	
+	@Override
+	protected void onBeforeRender() {
+//		final String srcTemplate = getMarkupAttributes().getString("src");
+//		final Map<String, Object> vars = ImmutableMap.<String, Object>of(
+//				"imagesUri", webAddress.getImagesUri(), "skinUri", webAddress.getSkinUri());
+//		log.trace("Expanding {} using {}", getPageRelativePath(), srcTemplate);
+//		try {
+//			final String src = UriTemplate.fromTemplate(srcTemplate).expand(vars);
+//			
+//			add(new AttributeModifier("src", src).);
+//		} catch (ExpressionParseException e) {
+//			throw new SiteException(e, "WebImage %'s src invalid URI template or without variables, please use standard img for non-dynamic URIs: %s",
+//					getPageRelativePath(), srcTemplate);
+//		}
+		super.onBeforeRender();
+	}
+
+}
