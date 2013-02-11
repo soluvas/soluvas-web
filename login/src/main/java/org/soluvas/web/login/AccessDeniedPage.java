7fb7742c2311fe81f2ddde83e0eb1b2b3a7f4fa8
Rudi Wijaya <rudi_mail90@yahoo.com>
2013-02-11 15:26:28 +0700

    Refs #484. Summary: 1. SalesOrder, Product, Shop and Person Pages are finish by dev.

---
+++ login/src/main/java/org/soluvas/web/login/AccessDeniedPage.java
@@ -0,0 +1,17 @@
+package org.soluvas.web.login;
+
+import org.ops4j.pax.wicket.api.PaxWicketMountPoint;
+import org.soluvas.web.bootstrap.BootstrapPage;
+
+/**
+ * @author rudi
+ *
+ */
+@SuppressWarnings("serial")
+@PaxWicketMountPoint(mountPoint="access_denied/")
+public class AccessDeniedPage extends BootstrapPage {
+	
+	public AccessDeniedPage() {
+	}
+
+}
