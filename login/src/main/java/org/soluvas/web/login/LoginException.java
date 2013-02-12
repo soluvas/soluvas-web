c45341834d8b8f1a7c3b10963fa084b0a4ee8fba
Atang Sutisna <atang@bippo.co.id>
2013-02-12 13:17:48 +0700

    support Java6

--- login/src/main/java/org/soluvas/web/login/LoginException.java
+++ login/src/main/java/org/soluvas/web/login/LoginException.java
@@ -7,11 +7,6 @@ public class LoginException extends RuntimeException {
 		super();
 	}
 
-	public LoginException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 	public LoginException(String message, Throwable cause) {
 		super(message, cause);
 	}
