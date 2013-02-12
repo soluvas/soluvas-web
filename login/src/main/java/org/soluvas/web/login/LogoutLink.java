f03909aed9f40de83d73d1ad99c2eaf4ea3da256
Rudi Wijaya <rudi_mail90@yahoo.com>
2013-02-12 16:39:02 +0700

    Refs #395. Summary: 1. Redirect to HomePage.

--- login/src/main/java/org/soluvas/web/login/LogoutLink.java
+++ login/src/main/java/org/soluvas/web/login/LogoutLink.java
@@ -1,14 +1,11 @@
 package org.soluvas.web.login;
 
-import java.util.Collection;
-
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
-import org.apache.wicket.Component;
 import org.apache.wicket.Page;
+import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxLink;
-import org.apache.wicket.model.IModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,13 +17,9 @@ import org.slf4j.LoggerFactory;
 public class LogoutLink extends IndicatingAjaxLink<Void> {
 
 	private static final Logger log = LoggerFactory.getLogger(LogoutLink.class);
-	private final Collection<Component> ajaxTargets;
-	private final Collection<IModel<?>> modelsToDetach;
 	
-	public LogoutLink(String id, Collection<Component> ajaxTargets, Collection<IModel<?>> modelsToDetach) {
+	public LogoutLink(String id) {
 		super(id);
-		this.ajaxTargets = ajaxTargets;
-		this.modelsToDetach = modelsToDetach;
 	}
 
 	@Override
@@ -36,11 +29,7 @@ public class LogoutLink extends IndicatingAjaxLink<Void> {
 		log.info("Logging out {} and redirecting to {}", currentUser.getPrincipal(), homePage);
 		currentUser.logout();
 		info("Anda telah log out.");
-		for (final IModel<?> model : modelsToDetach) {
-			model.detach();
-		}
-		target.add(ajaxTargets.toArray(new Component[] {}));
-//		getRequestCycle().setResponsePage(homePage);
+		throw new RestartResponseException(homePage);
 	}
 
 }
