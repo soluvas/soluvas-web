155c68061d819e0ffa54c0bd0ec0bcff160a5654
Ahmad Syarif Farsiado <farsiado@berbatik.com>
2013-03-18 15:18:40 +0700

    styling

---
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/BackendPage.java
@@ -0,0 +1,24 @@
+package org.soluvas.web.bootstrap;
+
+/**
+ * Only default styling.
+ * @author rio
+ */
+@SuppressWarnings("serial")
+public class BackendPage extends BootstrapPage {
+
+	/**
+	 * 
+	 */
+	public BackendPage() {
+		super();
+	}
+
+	/**
+	 * @param sidebarVisible
+	 */
+	public BackendPage(boolean sidebarVisible) {
+		super(sidebarVisible);
+	}
+
+}
