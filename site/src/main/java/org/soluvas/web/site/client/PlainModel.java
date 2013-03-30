0e683ad53615f94bf545f827920d26c85e8c9c62
Rudi Wijaya <rudi_mail90@yahoo.com>
2013-03-30 16:01:13 +0700

    product shop edit now showing, with JavaScript errors

--- site/src/main/java/org/soluvas/web/site/client/PlainModel.java
+++ site/src/main/java/org/soluvas/web/site/client/PlainModel.java
@@ -6,15 +6,9 @@ import org.apache.wicket.Component;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceReference;
-import org.soluvas.json.JacksonMapperFactory;
+import org.soluvas.json.JsonUtils;
 import org.soluvas.web.site.Page;
 import org.soluvas.web.site.SiteException;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Supplier;
 
 /**
  * Adds plain model instance with preloaded data.
@@ -67,17 +61,18 @@ public class PlainModel<T> extends JsSource {
 
 	@Override
 	public String getJsSource() {
-		final BundleContext bundleContext = FrameworkUtil.getBundle(PlainModel.class).getBundleContext();
-		final ServiceReference<JacksonMapperFactory> jacksonMapperFactoryRef = bundleContext.getServiceReference(JacksonMapperFactory.class);
-		final Supplier<ObjectMapper> jacksonMapperFactory = bundleContext.getService(jacksonMapperFactoryRef);
+//		final BundleContext bundleContext = FrameworkUtil.getBundle(PlainModel.class).getBundleContext();
+//		final ServiceReference<JacksonMapperFactory> jacksonMapperFactoryRef = bundleContext.getServiceReference(JacksonMapperFactory.class);
+//		final Supplier<ObjectMapper> jacksonMapperFactory = bundleContext.getService(jacksonMapperFactoryRef);
 		final T data = model.getObject();
 		try {
-			final ObjectMapper objectMapper = jacksonMapperFactory.get();
-			return name + " = " + objectMapper.writeValueAsString(data) + ";";
+//			final ObjectMapper objectMapper = jacksonMapperFactory.get();
+//			return name + " = " + objectMapper.writeValueAsString(data) + ";";
+			return name + " = " + JsonUtils.asJson(data) + ";";
 		} catch (Exception e) {
 			throw new SiteException("Cannot serialize model to JSON: " + name + " from " + data, e);
 		} finally {
-			bundleContext.ungetService(jacksonMapperFactoryRef);
+//			bundleContext.ungetService(jacksonMapperFactoryRef);
 		}
 	}
 	
