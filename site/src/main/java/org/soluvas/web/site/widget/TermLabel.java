69b77ba45c94781d7a0e4de091109180d3b78c4d
Haidar Mar'ie <haidar@bippo.co.id>
2013-05-08 14:43:29 +0700

    rubah img color with circle

--- site/src/main/java/org/soluvas/web/site/widget/TermLabel.java
+++ site/src/main/java/org/soluvas/web/site/widget/TermLabel.java
@@ -56,7 +56,7 @@ public class TermLabel extends Label {
 			if (term.getImageId() != null) {
 				final String bundleName = "base".equals(term.getNsPrefix()) ? "org.soluvas.data" : "tenant_common";
 				final String uri = webAddress.getImagesUri() + bundleName + "/" + term.getKindNsPrefix() + "_" + term.getKindName() +"/" + term.getImageId() + ".png";
-				iconHtml = "<img src=\"" + uri + "\" alt=\"" + Strings.escapeMarkup(term.getDisplayName()) + "\" title=\"" + org.apache.wicket.util.string.Strings.escapeMarkup(term.getDisplayName()) + "\"/> ";
+				iconHtml = "<img class=\"img-circle\" src=\"" + uri + "\" alt=\"" + Strings.escapeMarkup(term.getDisplayName()) + "\" title=\"" + org.apache.wicket.util.string.Strings.escapeMarkup(term.getDisplayName()) + "\"/> ";
 			} else {
 				final String color = term.getColor();
 				if (color != null) {
