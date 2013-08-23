c6d5ff44d8cf344e3d63b2e4a1b2c8bfc7f141dc
Rudi Wijaya <rudi_mail90@yahoo.com>
2013-08-23 15:17:27 +0700

    BC-1223 #resolve. Use AutoDisableAjaxButton

---
+++ site/src/main/java/org/soluvas/web/site/widget/AutoDisableAjaxButton.java
@@ -0,0 +1,40 @@
+package org.soluvas.web.site.widget;
+
+import org.apache.wicket.ajax.AjaxChannel;
+import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
+import org.apache.wicket.extensions.ajax.markup.html.IndicatingAjaxButton;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.model.IModel;
+
+/**
+ * {@link IndicatingAjaxButton} which auto-disables during AJAX request.
+ * @author rudi
+ */
+public class AutoDisableAjaxButton extends IndicatingAjaxButton {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AutoDisableAjaxButton(String id, Form<?> form) {
+		super(id, form);
+	}
+
+	public AutoDisableAjaxButton(String id, IModel<String> model, Form<?> form) {
+		super(id, model, form);
+	}
+
+	public AutoDisableAjaxButton(String id, IModel<String> model) {
+		super(id, model);
+	}
+
+	public AutoDisableAjaxButton(String id) {
+		super(id);
+	}
+
+	@Override
+	protected void updateAjaxAttributes(AjaxRequestAttributes attributes) {
+		super.updateAjaxAttributes(attributes);
+		attributes.setChannel(new AjaxChannel("autodisable", AjaxChannel.Type.ACTIVE));
+		attributes.getAjaxCallListeners().add(new AutoDisableAjaxCallListener());
+	}
+	
+}
---
+++ site/src/main/java/org/soluvas/web/site/widget/AutoDisableAjaxCallListener.java
@@ -0,0 +1,39 @@
+package org.soluvas.web.site.widget;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxChannel;
+import org.apache.wicket.ajax.attributes.AjaxCallListener;
+
+/**
+ * Disables the component via JavaScript before AJAX, and re-enables it again after AJAX success or failure.
+ * Note that you still need to use a {@link AjaxChannel.Type#ACTIVE} {@link AjaxChannel}, because the default
+ * is {@link AjaxChannel.Type#QUEUE}.
+ * 
+ * @see AutoDisableAjaxButton
+ * @author rudi
+ */
+public class AutoDisableAjaxCallListener extends AjaxCallListener {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public CharSequence getPrecondition(Component component) {
+//		return "console.log('PRE', $(this).attr('disabled')); if ($(this).attr('disabled') != undefined) return false;\n" + super.getPrecondition(component);
+		return "if ($(this).attr('disabled') != undefined) return false;\n" + super.getPrecondition(component);
+	}
+	
+	@Override
+	public CharSequence getBeforeSendHandler(
+			Component component) {
+//		return "console.log('beforeSEND');$(this).attr('disabled', 'disabled');\n" + super.getBeforeSendHandler(component);
+		return "$(this).attr('disabled', 'disabled');\n" + super.getBeforeSendHandler(component);
+	}
+
+	@Override
+	public CharSequence getCompleteHandler(
+			Component component) {
+//		return super.getCompleteHandler(component) + "\nconsole.log('AFTER');$(this).attr('disabled', " + (component.isEnabledInHierarchy() ? "null" : "'disabled'") + ");";
+		return super.getCompleteHandler(component) + "\n$(this).attr('disabled', " + (component.isEnabledInHierarchy() ? "null" : "'disabled'") + ");";
+	}
+
+}
