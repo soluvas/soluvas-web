99e86933fe452b045190d76846f359e71078016e
Ahmad Syarif Farsiado <farsiado@berbatik.com>
2013-09-05 17:59:23 +0700

    BC-1233 #resolve. Fixed.

---
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/ToTopJs.java
@@ -0,0 +1,14 @@
+package org.soluvas.web.bootstrap;
+
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
+
+/**
+ * @author rio
+ *
+ */
+public class ToTopJs {
+
+	public static final JavaScriptResourceReference EASING = new JavaScriptResourceReference(ToTopJs.class, "easing.js");
+	public static final JavaScriptResourceReference TO_TOP = new JavaScriptResourceReference(ToTopJs.class, "jquery.ui.totop.min.js");
+}
+
