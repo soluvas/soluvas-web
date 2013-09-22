e55ab3d0e887dda1e876f3defb5f9620293ab00d
Hendy Irawan <ceefour666@gmail.com>
2013-09-22 15:28:38 +0700

    RESOLVED - issue BC-1373: CategorizedProductPage & ProductsByReleasePage throw exception slugPath padahal URI benar https://idbippo.atlassian.net/browse/BC-1373

--- bootstrap/src/main/java/org/soluvas/web/bootstrap/PageNotFound.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/PageNotFound.java
@@ -3,8 +3,23 @@ package org.soluvas.web.bootstrap;
 import org.apache.wicket.request.http.WebResponse;
 
 /**
+ * Usage:
+ * 
+ * <pre>{@literal
+ * categoryModel = new LoadableDetachableModel<Category>() {
+ * 	@Override
+ * 	protected Category load() {
+ * 		final Category category = categoryRepo.findOneBySlugPath(slugPath, ImmutableSet.of(CategoryStatus.ACTIVE));
+ * 		if (category == null) {
+ * 			log.debug("Cannot find ACTIVE category with slug path '{}'", slugPath);
+ * 			throw new NonResettingRestartException(PageNotFound.class, null, RedirectPolicy.NEVER_REDIRECT);
+ * 		}
+ * 	return category;
+ * 	}
+ * };
+ * }</pre>
+ * 
  * @author atang
- *
  */
 public class PageNotFound extends BootstrapPage {
 
