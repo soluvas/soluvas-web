1d3361f22a3b19304e3b413c0b25ffd0e7e21077
Hendy Irawan <ceefour666@gmail.com>
2013-09-23 20:46:05 +0700

    OPEN - issue BC-1397 #resolve Hide LiveChatInc+newsletter+FB+twitter buttons+Google Analytics bila user mempunyai securityRole tingkat mall (sysadmin, admin, operator, dll.) https://idbippo.atlassian.net/browse/BC-1397

--- site/src/main/java/org/soluvas/web/googleanalytics/GoogleAnalyticsPanel.java
+++ site/src/main/java/org/soluvas/web/googleanalytics/GoogleAnalyticsPanel.java
@@ -6,9 +6,14 @@ import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.soluvas.security.SecurityCatalog;
 
 import com.google.common.base.Strings;
 
+/**
+ * @todo AFTER DECEMBER 2013 : Only use Google Analytics tracking if NOT has app/mall role: https://idbippo.atlassian.net/browse/BC-1397
+ * @author ceefour
+ */
 public class GoogleAnalyticsPanel extends Panel {
 
 	private static final long serialVersionUID = 1L;
@@ -17,10 +22,28 @@ public class GoogleAnalyticsPanel extends Panel {
 	
 	@SpringBean
 	private GoogleAnalyticsManager googleAnalyticsMgr;
+	@SpringBean
+	private SecurityCatalog securityCatalog;
 	
 	public GoogleAnalyticsPanel(String id) {
 		super(id);
+		boolean displayGoogleAnalytics = false;
 		if (googleAnalyticsMgr.isEnabled()) {
+			// TODO: AFTER DECEMBER 2013 :
+			// only use Google Analytics tracking if NOT has app/mall role: https://idbippo.atlassian.net/browse/BC-1397
+			displayGoogleAnalytics = true;
+			// disabled logic for now, make our "visitor count" still high
+//			final Subject subject = SecurityUtils.getSubject();
+//			if (!subject.isAuthenticated()) {
+//				// display newsletter form for guests
+//				displayGoogleAnalytics = true;
+//			} else {
+//				// display newsletter form for regular users ONLY
+//				displayGoogleAnalytics = !SecurityHelper.hasAnyRealRole(securityCatalog, subject);
+//			}
+		}
+
+		if (displayGoogleAnalytics) {
 			if (Strings.isNullOrEmpty(googleAnalyticsMgr.getTrackingId())) {
 				log.warn("Google Analytics Tracking ID must be set");
 				add(new EmptyPanel("trackerId"));
