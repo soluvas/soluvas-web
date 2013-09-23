76fd61c9c90450063506c0d5d89e11f954476311
Hendy Irawan <ceefour666@gmail.com>
2013-09-23 18:42:27 +0700

    OPEN - issue BC-1397: Hide LiveChatInc bila user mempunyai securityRole tingkat mall (sysadmin, admin, operator, dll.) https://idbippo.atlassian.net/browse/BC-1397

--- site/src/main/java/org/soluvas/web/olark/LiveChatPanel.java
+++ site/src/main/java/org/soluvas/web/olark/LiveChatPanel.java
@@ -1,34 +1,63 @@
 package org.soluvas.web.olark;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.soluvas.security.SecurityCatalog;
+import org.soluvas.security.util.SecurityHelper;
+import org.soluvas.web.site.SiteException;
 
 /**
  * @author atang
  * 
  */
-@SuppressWarnings("serial")
 public class LiveChatPanel extends Panel {
+	private static final long serialVersionUID = 1L;
+
+	@SpringBean
+	private SecurityCatalog securityCatalog;
+
 	/**
 	 * @param id
 	 * @param model
 	 */
 	public LiveChatPanel(String id, IModel<LiveChatManager> model) {
 		super(id, model);
-		Component inner = null;
 		final LiveChatManager liveChatMgr = model.getObject();
-		switch (liveChatMgr.getProvider()) {
-		case OLARK:
-			inner = new Olark("inner", new Model<>(liveChatMgr.getSiteId()));
-			break;
-		case LIVECHATINC:
-			inner = new LiveChatinc("inner", new Model<>(
-					liveChatMgr.getSiteId()));
-			break;
+		// only display live chat if NOT has app/mall role: https://idbippo.atlassian.net/browse/BC-1397
+		boolean displayLiveChat = false;
+		if (liveChatMgr.isEnabled()) {
+			final Subject subject = SecurityUtils.getSubject();
+			if (!subject.isAuthenticated()) {
+				// display live chat for guests
+				displayLiveChat = true;
+			} else {
+				// display live chat for regular users ONLY
+				displayLiveChat = !SecurityHelper.hasAnyRealRole(securityCatalog, subject);
+			}
 		}
-		setVisible(liveChatMgr.isEnabled());
+		final Component inner;
+		if (displayLiveChat) {
+			switch (liveChatMgr.getProvider()) {
+			case OLARK:
+				inner = new Olark("inner", new Model<>(liveChatMgr.getSiteId()));
+				break;
+			case LIVECHATINC:
+				inner = new LiveChatinc("inner", new Model<>(
+						liveChatMgr.getSiteId()));
+				break;
+			default:
+				throw new SiteException("Unknown live chat provider: " + liveChatMgr.getProvider());
+			}
+		} else {
+			inner = new EmptyPanel("inner");
+			inner.setVisible(false);
+		}
 		add(inner);
 	}
 
