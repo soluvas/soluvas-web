ad2ea5d09693d96900675c81ca9c2ed69b2888ab
Hendy Irawan <ceefour666@gmail.com>
2013-12-10 07:29:56 -0430

    GRD-80 TimeTextField

---
+++ site/src/main/java/org/soluvas/web/site/widget/TimeTextField.java
@@ -0,0 +1,41 @@
+package org.soluvas.web.site.widget;
+
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.joda.time.LocalTime;
+
+/**
+ * input type="time". Uses String because not guaranteed that {@link LocalTime} would work,
+ * see {@link #getModelObjectAsLocalTime()}.
+ * @see #getModelObjectAsLocalTime()
+ * @author ceefour
+ */
+public class TimeTextField extends TextField<String> {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @param id
+	 */
+	public TimeTextField(String id) {
+		super(id, String.class);
+	}
+
+	/**
+	 * @param id
+	 * @param model
+	 */
+	public TimeTextField(String id, IModel<String> model) {
+		super(id, model, String.class);
+	}
+
+	@Override
+	protected String getInputType() {
+		return "time";
+	}
+	
+	public LocalTime getModelObjectAsLocalTime() {
+		return getModelObject() != null ? new LocalTime(getModelObject()) : null;
+	}
+
+}
