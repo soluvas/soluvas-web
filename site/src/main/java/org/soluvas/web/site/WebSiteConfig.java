14b081c9e227b28d65a18f44fd2fa9e25f95e081
Hendy Irawan <ceefour666@gmail.com>
2014-03-01 09:23:28 -0430

    OPEN - issue BC-1668: Multitenant architecture https://idbippo.atlassian.net/browse/BC-1668

--- site/src/main/java/org/soluvas/web/site/WebSiteConfig.java
+++ site/src/main/java/org/soluvas/web/site/WebSiteConfig.java
@@ -16,7 +16,7 @@ import com.google.common.collect.ImmutableSet;
  */
 @Configuration @Lazy
 @Import({LiveChatConfig.class, GoogleAnalyticsConfig.class})
-@ComponentScan({"org.soluvas.web.bootstrap"})
+@ComponentScan({"org.soluvas.web.site.rs", "org.soluvas.web.site.shell", "org.soluvas.web.bootstrap"})
 public class WebSiteConfig {
 
 	@Bean
