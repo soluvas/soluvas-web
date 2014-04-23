4b08f2b9f3cac0ad4c62a08c8debdc772b271b1e
Rudi Wijaya <rudi_mail90@yahoo.com>
2014-04-23 05:28:14 -0430

    BC-1773 #comment MixinSelect2

--- bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/MixinSelect2.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/MixinSelect2.java
@@ -2,15 +2,24 @@ package org.soluvas.web.bootstrap.widget;
 
 import java.util.Collection;
 
+import javax.annotation.Nullable;
+
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.injection.Injector;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.json.JSONException;
 import org.json.JSONWriter;
 import org.soluvas.data.Mixin;
 import org.soluvas.data.MixinManager;
+import org.soluvas.data.domain.Page;
+import org.soluvas.data.domain.PageRequest;
 import org.soluvas.web.site.EmfModel;
 
+import com.google.common.base.Function;
+import com.google.common.collect.FluentIterable;
 import com.vaynberg.wicket.select2.ChoiceProvider;
 import com.vaynberg.wicket.select2.Response;
 import com.vaynberg.wicket.select2.Select2Choice;
@@ -34,21 +43,31 @@ public class MixinSelect2 extends Select2Choice<Mixin> {
 
 		@Override
 		public void query(String term, int page, Response<Mixin> response) {
-			// TODO Auto-generated method stub
-			
+			final Page<Mixin> pagedMixin = mixinMgr.findMixin(term.trim(), new PageRequest(page, 20));
+			response.addAll(pagedMixin.getContent());
+			response.setHasMore(!pagedMixin.hasNextPage());
 		}
 
 		@Override
-		public void toJson(Mixin choice, JSONWriter writer)
-				throws JSONException {
-			// TODO Auto-generated method stub
-			
+		public void toJson(Mixin choice, JSONWriter writer) throws JSONException {
+			writer.key("id").value(choice.getQName())
+				.key("text").value(choice.getDisplayName())
+				.key("name").value(choice.getName());
 		}
 
 		@Override
 		public Collection<Mixin> toChoices(Collection<String> ids) {
-			// TODO Auto-generated method stub
-			return null;
+			return FluentIterable.from(ids).transform(new Function<String, Mixin>() {
+				@Override @Nullable
+				public Mixin apply(@Nullable String input) {
+					return mixinMgr.findMixin(input);
+				}
+			}).toList();
+		}
+		
+		@Override
+		public void detach() {
+			super.detach();
 		}
 		
 	}
@@ -61,7 +80,24 @@ public class MixinSelect2 extends Select2Choice<Mixin> {
 		super(id, mixinModel, new MixinChoiceProvider());
 	}
 
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+	}
 	
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+		add(new AttributeAppender("class", new Model<>("input-xxlarge"), " "));
+		getSettings().getAjax().setQuietMillis(250);
+		getSettings().setFormatResult(
+				"function(object, container, query, escapeMarkup) {" +
+				"var textMarkup = []; window.Select2.util.markMatch(object.text, query.term, textMarkup, escapeMarkup);" +
+				"var thediv = $('<div>').css({marginLeft: '24px', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'})" +
+				"  .append(textMarkup.join('')).append(' ').append($('<small>').text(object.name));" +
+				"container.append(thediv);" +
+				"}");
+	}
 	
 
 }
