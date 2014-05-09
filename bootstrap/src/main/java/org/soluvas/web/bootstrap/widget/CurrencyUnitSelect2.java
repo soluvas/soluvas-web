69d250630cf1cd572bf07cdbbbeee03ee659065c
Hendy Irawan <ceefour666@gmail.com>
2014-05-09 12:08:41 -0430

    OPEN - issue BC-1667: Automated Tenant Creation https://idbippo.atlassian.net/browse/BC-1667

--- bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/CurrencyUnitSelect2.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/CurrencyUnitSelect2.java
@@ -1,8 +1,8 @@
 package org.soluvas.web.bootstrap.widget;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Locale;
+import java.util.Currency;
+import java.util.List;
 
 import javax.annotation.Nullable;
 
@@ -14,9 +14,9 @@ import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.joda.money.CurrencyUnit;
 import org.json.JSONException;
 import org.json.JSONWriter;
-import org.soluvas.commons.locale.LocaleOrdering;
 import org.soluvas.data.domain.PageRequest;
 import org.soluvas.geo.CityRepository;
 import org.soluvas.web.site.FlagsCssResourceReference;
@@ -24,74 +24,71 @@ import org.soluvas.web.site.FlagsCssResourceReference;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.vaynberg.wicket.select2.ChoiceProvider;
 import com.vaynberg.wicket.select2.Response;
 import com.vaynberg.wicket.select2.Select2Choice;
 
 /**
- * {@link Select2Choice} UI component for {@link Locale}.
+ * {@link Select2Choice} UI component for {@link CurrencyUnit}.
  * @author ceefour
  */
-public class LocaleSelect2 extends Select2Choice<Locale> {
+public class CurrencyUnitSelect2 extends Select2Choice<CurrencyUnit> {
 
 	private static final long serialVersionUID = 1L;
 	
-	private static class LocaleChoiceProvider extends ChoiceProvider<Locale> {
+	private static class CurrencyUnitChoiceProvider extends ChoiceProvider<CurrencyUnit> {
 
 		private static final long serialVersionUID = 1L;
-		/**
-		 * static cache to make it faster to search
-		 */
-		private static final ImmutableList<Locale> sorted = 
-				LocaleOrdering.INSTANCE.immutableSortedCopy(Arrays.asList(Locale.getAvailableLocales()));
 
 		@SpringBean
 		private CityRepository cityRepo;
 		
-		public LocaleChoiceProvider() {
+		public CurrencyUnitChoiceProvider() {
 			super();
 			Injector.get().inject(this);
 		}
 
 		@Override
-		public void query(String term, int page, Response<Locale> response) {
+		public void query(String term, int page, Response<CurrencyUnit> response) {
 			final String trimmedTerm = term.trim();
 			final PageRequest pageable = new PageRequest(page, 20);
-			final FluentIterable<Locale> localesPlus = FluentIterable.from(sorted)
-				.filter(new Predicate<Locale>() {
+			final List<CurrencyUnit> sorted = CurrencyUnit.registeredCurrencies();
+//			final ImmutableList<CurrencyUnit> sorted = CurrencyUnitOrdering.INSTANCE.immutableSortedCopy(Arrays.asList(CurrencyUnit.getAvailableCurrencyUnits()));
+			final FluentIterable<CurrencyUnit> currenciesPlus = FluentIterable.from(sorted)
+				.filter(new Predicate<CurrencyUnit>() {
 					@Override
-					public boolean apply(@Nullable Locale locale) {
-						final String searchable = locale.toLanguageTag() + " " + locale.getDisplayName();
+					public boolean apply(@Nullable CurrencyUnit currency) {
+						final String searchable = currency + " " + currency.getSymbol();
 						return StringUtils.containsIgnoreCase(searchable, trimmedTerm);
 					}
 				}).skip((int) pageable.getOffset());
-			response.addAll(localesPlus.limit(20).toList());
-			response.setHasMore(localesPlus.limit(21).size() > 20);
+			response.addAll(currenciesPlus.limit(20).toList());
+			response.setHasMore(currenciesPlus.limit(21).size() > 20);
 		}
 
 		@Override
-		public Collection<Locale> toChoices(Collection<String> ids) {
-			return FluentIterable.from(ids).transform(new Function<String, Locale>() {
+		public Collection<CurrencyUnit> toChoices(Collection<String> ids) {
+			return FluentIterable.from(ids).transform(new Function<String, CurrencyUnit>() {
 				@Override @Nullable
-				public Locale apply(@Nullable String input) {
-					return Locale.forLanguageTag(input);
+				public CurrencyUnit apply(@Nullable String input) {
+					return CurrencyUnit.of(input);
 				}
 			}).toSet();
 //			// Workaround for Select2Choice "bug": https://github.com/ivaynberg/wicket-select2/issues/56
 //			if (!ids.isEmpty() && found.isEmpty()) {
-//				return Arrays.asList(new Locale[] { null });
+//				return Arrays.asList(new CurrencyUnit[] { null });
 //			} else {
 //				return found;
 //			}
 		}
 		
 		@Override
-		public void toJson(Locale choice, JSONWriter writer)
+		public void toJson(CurrencyUnit choice, JSONWriter writer)
 				throws JSONException {
-			writer.key("id").value(choice.toLanguageTag())
-				.key("text").value(choice.getDisplayName())
-				.key("c").value(choice.getCountry());
+			writer.key("id").value(choice.getCode())
+				.key("text").value(Currency.getInstance(choice.getCode()).getDisplayName())
+				.key("c").value(Iterables.getFirst(choice.getCountryCodes(), null));
 		}
 		
 		@Override
@@ -101,12 +98,12 @@ public class LocaleSelect2 extends Select2Choice<Locale> {
 		
 	}
 
-	public LocaleSelect2(String id, IModel<Locale> model) {
-		super(id, model, new LocaleChoiceProvider());
+	public CurrencyUnitSelect2(String id, IModel<CurrencyUnit> model) {
+		super(id, model, new CurrencyUnitChoiceProvider());
 	}
 
-	public LocaleSelect2(String id) {
-		super(id, new Model<Locale>(), new LocaleChoiceProvider());
+	public CurrencyUnitSelect2(String id) {
+		super(id, new Model<CurrencyUnit>(), new CurrencyUnitChoiceProvider());
 	}
 	
 	@Override
@@ -136,12 +133,16 @@ public class LocaleSelect2 extends Select2Choice<Locale> {
 			"container.append(' ');" +
 			"var textMarkup = []; window.Select2.util.markMatch(object.text, query.term, textMarkup, escapeMarkup);" +
 			"var thediv = $('<div>').css({marginLeft: '24px', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'})" +
+			"  .append($('<span>').attr({'class': 'label label-default', 'style': 'display: inline-block; margin-right: 0.2em; vertical-align: text-top; width: 4em'}).text(object.id))" +
+			"  .append(' ')" +
 			"  .append(textMarkup.join(''));" +
 			"container.append(thediv);" +
 			"}");
 		getSettings().setFormatSelection(
 				"function(object, container, query) {" +
 				"container.append($('<span>').attr({'class': 'flag flag-' + object.c.toLowerCase(), 'title': object.text}));" +
+				"container.append(' ');" +
+				"container.append($('<span>').attr({'class': 'label label-default', 'style': 'display: inline-block; margin-right: 0.2em; vertical-align: text-top; width: 4em'}).text(object.id));" +
 				"container.append(' ');" +
 				"container.append(document.createTextNode(object.text));" +
 				"}");
