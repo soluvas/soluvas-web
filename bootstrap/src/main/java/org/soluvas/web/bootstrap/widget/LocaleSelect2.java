9ff9dbf30a8fe3ed3959dab136e90717a9bbd5ae
Hendy Irawan <ceefour666@gmail.com>
2014-05-09 10:54:40 -0430

    OPEN - issue BC-1667: Automated Tenant Creation https://idbippo.atlassian.net/browse/BC-1667

---
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/LocaleSelect2.java
@@ -0,0 +1,146 @@
+package org.soluvas.web.bootstrap.widget;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Locale;
+
+import javax.annotation.Nullable;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.injection.Injector;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.json.JSONException;
+import org.json.JSONWriter;
+import org.soluvas.commons.locale.LocaleOrdering;
+import org.soluvas.data.domain.PageRequest;
+import org.soluvas.geo.CityRepository;
+import org.soluvas.web.site.FlagsCssResourceReference;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.vaynberg.wicket.select2.ChoiceProvider;
+import com.vaynberg.wicket.select2.Response;
+import com.vaynberg.wicket.select2.Select2Choice;
+
+/**
+ * {@link Select2Choice} UI component for {@link Locale}.
+ * @author ceefour
+ */
+public class LocaleSelect2 extends Select2Choice<Locale> {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static class LocaleChoiceProvider extends ChoiceProvider<Locale> {
+
+		private static final long serialVersionUID = 1L;
+
+		@SpringBean
+		private CityRepository cityRepo;
+		
+		public LocaleChoiceProvider() {
+			super();
+			Injector.get().inject(this);
+		}
+
+		@Override
+		public void query(String term, int page, Response<Locale> response) {
+			final String trimmedTerm = term.trim();
+			final PageRequest pageable = new PageRequest(page, 20);
+			final ImmutableList<Locale> sorted = LocaleOrdering.INSTANCE.immutableSortedCopy(Arrays.asList(Locale.getAvailableLocales()));
+			final FluentIterable<Locale> localesPlus = FluentIterable.from(sorted)
+				.filter(new Predicate<Locale>() {
+					@Override
+					public boolean apply(@Nullable Locale locale) {
+						final String searchable = locale.toLanguageTag() + " " + locale.getDisplayName();
+						return StringUtils.containsIgnoreCase(searchable, trimmedTerm);
+					}
+				}).skip((int) pageable.getOffset());
+			response.addAll(localesPlus.limit(20).toList());
+			response.setHasMore(localesPlus.limit(21).size() > 20);
+		}
+
+		@Override
+		public Collection<Locale> toChoices(Collection<String> ids) {
+			return FluentIterable.from(ids).transform(new Function<String, Locale>() {
+				@Override @Nullable
+				public Locale apply(@Nullable String input) {
+					return Locale.forLanguageTag(input);
+				}
+			}).toSet();
+//			// Workaround for Select2Choice "bug": https://github.com/ivaynberg/wicket-select2/issues/56
+//			if (!ids.isEmpty() && found.isEmpty()) {
+//				return Arrays.asList(new Locale[] { null });
+//			} else {
+//				return found;
+//			}
+		}
+		
+		@Override
+		public void toJson(Locale choice, JSONWriter writer)
+				throws JSONException {
+			writer.key("id").value(choice.toLanguageTag())
+				.key("text").value(choice.getDisplayName())
+				.key("c").value(choice.getCountry());
+		}
+		
+		@Override
+		public void detach() {
+			super.detach();
+		}
+		
+	}
+
+	public LocaleSelect2(String id, IModel<Locale> model) {
+		super(id, model, new LocaleChoiceProvider());
+	}
+
+	public LocaleSelect2(String id) {
+		super(id, new Model<Locale>(), new LocaleChoiceProvider());
+	}
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		response.render(CssHeaderItem.forReference(FlagsCssResourceReference.INSTANCE));
+	}
+	
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
+		add(new AttributeAppender("class", new Model<>("input-xlarge"), " "));
+		getSettings().getAjax().setQuietMillis(400);
+//		 <span class="flag flag-cz" title="Czech Republic" />
+//		getSettings().setFormatResult(
+//			"function(object, container, query, escapeMarkup) {" +
+//			"container.append($('<span>').attr('class', 'flag flag-' + object.toLowerCase()).attr('title', object.text));" +		
+//			"container.append(' ');" +
+//			"var textMarkup = []; window.Select2.util.markMatch(object.text, query.term, textMarkup, escapeMarkup);" +
+//			"var thediv = $('<span>')" +
+//			"  .append(textMarkup.join(''));" +
+//			"container.append(thediv);" +
+//			"}");
+		getSettings().setFormatResult(
+			"function(object, container, query, escapeMarkup) {" +
+			"container.append($('<span>').css({float: 'left', marginTop: '4px'}).attr({class: 'flag flag-' + object.c.toLowerCase(), title: object.text}));" +
+			"container.append(' ');" +
+			"var textMarkup = []; window.Select2.util.markMatch(object.text, query.term, textMarkup, escapeMarkup);" +
+			"var thediv = $('<div>').css({marginLeft: '24px', textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap'})" +
+			"  .append(textMarkup.join(''));" +
+			"container.append(thediv);" +
+			"}");
+		getSettings().setFormatSelection(
+				"function(object, container, query) {" +
+				"container.append($('<span>').attr({'class': 'flag flag-' + object.c.toLowerCase(), 'title': object.text}));" +
+				"container.append(' ');" +
+				"container.append(document.createTextNode(object.text));" +
+				"}");
+	}
+
+}
