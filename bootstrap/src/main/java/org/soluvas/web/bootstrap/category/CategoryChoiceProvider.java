4e2e2249680007a6b5a5f8458a8c873aa382e8d3
Rudi Wijaya <rudi_mail90@yahoo.com>
2014-09-22 17:59:44 +0700

    BC-2223 #resolve. for missed category...

--- bootstrap/src/main/java/org/soluvas/web/bootstrap/category/CategoryChoiceProvider.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/category/CategoryChoiceProvider.java
@@ -9,31 +9,35 @@ import javax.inject.Inject;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.injection.Injector;
 import org.apache.wicket.model.IModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.soluvas.category.Category;
 import org.soluvas.category.CategoryRepository;
-import org.soluvas.commons.IdFunction;
+import org.soluvas.commons.NotNullPredicate;
 
 import com.google.common.base.Function;
-import com.google.common.base.Objects;
 import com.google.common.base.Predicate;
 import com.google.common.base.Strings;
 import com.google.common.collect.Collections2;
-import com.google.common.collect.Iterables;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.Lists;
 import com.vaynberg.wicket.select2.Response;
 import com.vaynberg.wicket.select2.TextChoiceProvider;
 
 @SuppressWarnings("serial")
 class CategoryChoiceProvider extends TextChoiceProvider<Category> {
+	
+	private static final Logger log = LoggerFactory
+			.getLogger(CategoryChoiceProvider.class);
 		
 	@Inject
 	private CategoryRepository categoryRepo;
 	private final IModel<List<Category>> sortedCategoriesModel;
 	
-	public CategoryChoiceProvider(IModel<List<Category>> sortedCategoriesModel) {
+	public CategoryChoiceProvider(IModel<List<Category>> sortedActiveCategoriesModel) {
 		super();
 		Injector.get().inject(this);
-		this.sortedCategoriesModel = sortedCategoriesModel;
+		this.sortedCategoriesModel = sortedActiveCategoriesModel;
 	}
 	
 	@Override
@@ -50,7 +54,7 @@ class CategoryChoiceProvider extends TextChoiceProvider<Category> {
 		if (!Strings.isNullOrEmpty(child.getParentUName())) {
 			final Category parent = categoryRepo.findOne(child.getParentUName());
 			if (parent != null) {
-				CategorySelect2MultiChoice.log.debug("Parent of {} is {}", child.getUName(), parent.getUName());
+				log.debug("Parent of {} is {}", child.getUName(), parent.getUName());
 				return getParentPrefix(parent) + "" + parent.getName() + " > ";
 			}
 			return "";
@@ -61,12 +65,12 @@ class CategoryChoiceProvider extends TextChoiceProvider<Category> {
 
 	@Override
 	protected Object getId(Category choice) {
-		return choice.getId();
+		return choice.getUName();
 	}
 
 	@Override
 	public void query(final String term, int page, Response<Category> response) {
-		final Collection<Category> catList = Collections2.filter(sortedCategoriesModel.getObject(), new Predicate<Category>(){
+		final Collection<Category> catList = Collections2.filter(sortedCategoriesModel.getObject(), new Predicate<Category>() {
 			@Override
 			public boolean apply(@Nullable Category input) {
 				return StringUtils.containsIgnoreCase(input.getName(), term.trim());
@@ -77,25 +81,27 @@ class CategoryChoiceProvider extends TextChoiceProvider<Category> {
 
 	@Override
 	public Collection<Category> toChoices(Collection<String> ids) {
-		return Collections2.transform(ids, new Function<String, Category>() {
+		return FluentIterable.from(ids).transform(new Function<String, Category>() {
 			@Override @Nullable
-			public Category apply(@Nullable final String id) {
-				CategorySelect2MultiChoice.log.debug("id is {}. Sorted category has {} records", id, sortedCategoriesModel.getObject().size());
-				try {
-					return Iterables.find(sortedCategoriesModel.getObject(), new Predicate<Category>() {
-						@SuppressWarnings("null") @Override
-						public boolean apply(@Nullable Category cat) {
-							CategorySelect2MultiChoice.log.debug("cat is {}", cat);
-							return Objects.equal(cat.getId(), id);
-						}
-					}, null);
-				} catch (Exception e) {
-					CategorySelect2MultiChoice.log.warn("Invalid category ID '{}', {} valid categories are: {}",
-							id, sortedCategoriesModel.getObject().size(), Lists.transform(sortedCategoriesModel.getObject(), new IdFunction()));
-					return null;
+			public Category apply(@Nullable final String uName) {
+				log.trace("Category UName is {}. Sorted category has {} records", uName, sortedCategoriesModel.getObject().size());
+				@Nullable
+				Category found = categoryRepo.findOne(uName);
+				if (found == null) {
+					log.warn("Invalid category UName '{}', {} valid category UNames are: {}",
+							uName, sortedCategoriesModel.getObject().size(), 
+							Lists.transform(sortedCategoriesModel.getObject(), new Function<Category, String>() {
+								@Override
+								public String apply(Category input) {
+									return input.getUName();
+								}
+							}));
 				}
+				return found;
 			}
-		});
+		})
+		.filter(new NotNullPredicate<>())
+		.toList();
 	}
 	
 }
--- bootstrap/src/main/java/org/soluvas/web/bootstrap/category/CategorySelect2MultiChoice.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/category/CategorySelect2MultiChoice.java
@@ -26,10 +26,15 @@ import org.soluvas.data.domain.Sort.Direction;
 import org.soluvas.image.ImageManager;
 import org.soluvas.web.bootstrap.widget.InteractiveSelect2MultiChoice;
 
+import com.google.common.base.Function;
 import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Ordering;
+import com.google.common.collect.Sets;
+import com.google.common.collect.Sets.SetView;
 
 /**
  * @author atang
@@ -45,12 +50,12 @@ public class CategorySelect2MultiChoice extends
 	private CategoryRepository categoryRepo;
 	@SpringBean
 	private ImageManager imageMgr;
-	private LoadableDetachableModel<List<Category>> sortedCategoriesModel;
+	private LoadableDetachableModel<List<Category>> sortedActiveCategoriesModel;
 //	private final IModel<List<Category>> selectedCategoryModel;
 	
 	private class LoadableCategoriesModel extends LoadableDetachableModel<Collection<Category>> {
 
-		private final ArrayList<String> categoryUNames = new ArrayList<>();
+		private volatile ArrayList<String> categoryUNames = new ArrayList<>();
 		
 		public LoadableCategoriesModel(@Nullable Collection<Category> currentCategories) {
 			super();
@@ -61,26 +66,48 @@ public class CategorySelect2MultiChoice extends
 
 		@Override
 		protected List<Category> load() {
-			return new ArrayList<>( categoryRepo.findAll(categoryUNames) );
+			final List<Category> foundCategories = categoryRepo.findAll(categoryUNames);
+			final ImmutableSet<String> foundCategoryUNames = FluentIterable.from(foundCategories).transform(new Function<Category, String>() {
+				@Override
+				public String apply(Category input) {
+					return input.getUName();
+				}
+			}).toSet();
+			final SetView<String> missingUNames = Sets.difference(ImmutableSet.copyOf(categoryUNames), foundCategoryUNames);
+			if (!missingUNames.isEmpty()) {
+				log.warn("Category multi-select2 wants {} ACTIVE categories ({}) but found {}, missing {} categories: {}",
+						categoryUNames.size(), categoryUNames, foundCategoryUNames.size(), missingUNames.size(), missingUNames);
+			}
+			return new ArrayList<>( foundCategories );
 		}
 		
 		@Override
 		public void detach() {
 			categoryUNames.clear();
-			if (getObject() != null) {
+			if (isAttached()) {
+				log.debug("Detaching {} categories", getObject().size());
 				for (Category category : getObject()) {
-					categoryUNames.add(category.getUName());
+					if (category != null) {
+						categoryUNames.add(category.getUName());
+					} else {
+						log.debug("Attempting to add null Category, that's weird?");
+					}
 				}
-				super.detach();
 			}
+			super.detach();
 		}
 		
 	}
 
+	/**
+	 * @param id
+	 * @param currentCategories Initially selected categories. These will be added into the component's
+	 * 		model using internal {@link LoadableCategoriesModel}.
+	 */
 	public CategorySelect2MultiChoice(String id, Collection<Category> currentCategories) {
 		super(id);
 		setModel(new LoadableCategoriesModel(currentCategories));
-		sortedCategoriesModel = new LoadableDetachableModel<List<Category>>() {
+		sortedActiveCategoriesModel = new LoadableDetachableModel<List<Category>>() {
 			@Override
 			protected List<Category> load() {
 				final List<Category> categoryList = categoryRepo.findAllByStatus(ImmutableList.of(CategoryStatus.ACTIVE), new PageRequest(0, 500, Direction.ASC, "name")).getContent();
@@ -102,11 +129,11 @@ public class CategorySelect2MultiChoice extends
 			}
 		};
 //		log.debug("sortedCategoriesModel has {} rows ", sortedCategoriesModel.getObject().size());
-		setProvider(new CategoryChoiceProvider(sortedCategoriesModel));
+		setProvider(new CategoryChoiceProvider(sortedActiveCategoriesModel));
 	}
 	
 	public IModel<List<Category>> getSortedCategoriesModel() {
-		return sortedCategoriesModel;
+		return sortedActiveCategoriesModel;
 	}
 	
 	@Override
@@ -118,10 +145,9 @@ public class CategorySelect2MultiChoice extends
 	}
 	
 	@Override
-	protected void detachModel() {
-//		getModel().detach();
-//		super.detachModel();
-		sortedCategoriesModel.detach();
+	protected void onDetach() {
+		sortedActiveCategoriesModel.detach();
+		super.onDetach();
 	}
 	
 }
