040d08b7cf8e65d4acd803b5c77fb05b35f71182
Hendy Irawan <ceefour666@gmail.com>
2014-12-09 07:44:16 -0430

    BC-2427: Replace Wicket error page dengan stack trace exception dengan yang lebih friendly
    Task-Url: https://idbippo.atlassian.net/browse/BC-2427

---
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/FriendlyErrorPage.java
@@ -0,0 +1,113 @@
+package org.soluvas.web.bootstrap;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collections;
+import java.util.UUID;
+
+import javax.inject.Inject;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.wicket.WicketRuntimeException;
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.filter.HeaderResponseContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.pages.InternalErrorPage;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.springframework.core.env.Environment;
+
+import com.google.common.base.Throwables;
+
+import de.agilecoders.wicket.core.markup.html.bootstrap.html.HtmlTag;
+import de.agilecoders.wicket.core.markup.html.references.BootstrapJavaScriptReference;
+import de.agilecoders.wicket.core.markup.html.themes.bootstrap.BootstrapCssReference;
+
+/**
+ * Human-friendly and mobile first {@link InternalErrorPage}
+ * with Bootstrap layout.
+ * @author ceefour
+ */
+@SuppressWarnings("serial")
+public class FriendlyErrorPage extends InternalErrorPage {
+
+	@Inject
+	private Environment env;
+	
+	@Override
+	public void renderHead(IHeaderResponse response) {
+		super.renderHead(response);
+		// do not depend on BootstrapSettings, because they will use ThemeProvider, which use custom theme which can be problematic or no bootstrap CSS at all
+		response.render(JavaScriptHeaderItem.forReference(BootstrapJavaScriptReference.instance()));
+		response.render(CssHeaderItem.forReference(BootstrapCssReference.instance()));
+	}
+	
+	public FriendlyErrorPage(UUID incidentId, Exception ex) {
+		super();
+		add(new HtmlTag("html", getRequest().getLocale()));
+		add(new HeaderResponseContainer("footer-container", "footer-container"));
+//		add(new BootstrapBaseBehavior());
+
+		Throwable realCause = ex;
+		while (true) {
+			if ((realCause instanceof WicketRuntimeException || realCause instanceof InvocationTargetException) && realCause.getCause() != null) {
+				realCause = realCause.getCause();
+				continue;
+			}
+			break;
+		}
+		add(new Label("exceptionName", realCause.getClass().getSimpleName()));
+		add(new Label("incidentId2", incidentId));
+		add(new Label("incidentId", incidentId));
+		final DateTimeZone timeZone = DateTimeZone.forID(env.getProperty("defaultTimeZone", DateTimeZone.getDefault().getID()));
+		add(new Label("time", new DateTime(timeZone)));
+		final HttpServletRequest httpRequest = (HttpServletRequest) getRequest().getContainerRequest();
+		String requestDetails =
+				"Auth type: " + httpRequest.getAuthType() + "\n"
+						+ "Character encoding: " + httpRequest.getCharacterEncoding() + "\n"
+						+ "Content length: " + httpRequest.getContentLength() + "\n" 
+						+ "Content type: " + httpRequest.getContentType() + "\n" 
+						+ "Context path: " + httpRequest.getContextPath() + "\n" 
+						+ "Local address: " + httpRequest.getLocalAddr() + "\n" 
+						+ "Local name: " + httpRequest.getLocalName() + "\n" 
+						+ "Local port: " + httpRequest.getLocalPort() + "\n" 
+						+ "Locale: " + ( httpRequest.getLocale() != null ? httpRequest.getLocale().toLanguageTag() : null) + "\n" 
+						+ "Method: " + httpRequest.getMethod() + "\n" 
+						+ "Path info: " + httpRequest.getPathInfo() + "\n" 
+						+ "Path translated: " + httpRequest.getPathTranslated() + "\n" 
+						+ "Protocol: " + httpRequest.getProtocol() + "\n" 
+						+ "Query string: " + httpRequest.getQueryString() + "\n" 
+						+ "Remote address: " + httpRequest.getRemoteAddr() + "\n" 
+						+ "Remote host: " + httpRequest.getRemoteHost() + "\n" 
+						+ "Remote port: " + httpRequest.getRemotePort() + "\n" 
+						+ "Remote user: " + httpRequest.getRemoteUser() + "\n" 
+						+ "Requested session ID: " + httpRequest.getRequestedSessionId() + "\n" 
+						+ "Request URI: " + httpRequest.getRequestURI() + "\n" 
+						+ "Request URL: " + httpRequest.getRequestURL() + "\n" 
+						+ "Scheme: " + httpRequest.getScheme() + "\n" 
+						+ "Server name: " + httpRequest.getServerName() + "\n" 
+						+ "Server port: " + httpRequest.getServerPort() + "\n" 
+						+ "Servlet path: " + httpRequest.getServletPath() + "\n" 
+						+ "User principal: " + httpRequest.getUserPrincipal() + "\n";
+		add(new Label("request", requestDetails));
+		
+		String cookies = "";
+		for (final Cookie cookie : httpRequest.getCookies()) {
+			cookies += cookie.getName() + ": " + cookie.getValue() + "; maxAge=" + cookie.getMaxAge()
+					+ "; domain=" + cookie.getDomain() + "; path=" + cookie.getPath() + "; version=" + cookie.getVersion()
+					+ "; secure=" + cookie.getSecure() + "; comment=" + cookie.getComment() + "\n";
+		}
+		add(new Label("cookies", cookies));
+		
+		String headers = "";
+		for (final String headerName : Collections.list(httpRequest.getHeaderNames())) {
+			headers += headerName + ": " + httpRequest.getHeader(headerName) + "\n";
+		}
+		add(new Label("headers", headers));
+		
+		add(new Label("stackTrace", Throwables.getStackTraceAsString(ex)));
+	}
+
+}
---
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/FriendlyErrorRequestListener.java
@@ -0,0 +1,37 @@
+package org.soluvas.web.bootstrap;
+
+import java.util.UUID;
+
+import org.apache.wicket.core.request.handler.PageProvider;
+import org.apache.wicket.core.request.handler.RenderPageRequestHandler;
+import org.apache.wicket.core.request.handler.RenderPageRequestHandler.RedirectPolicy;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.IRequestHandler;
+import org.apache.wicket.request.cycle.AbstractRequestCycleListener;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.soluvas.commons.tenant.CommandRequestAttributes;
+
+/**
+ * Should be added as the <b>last</b> {@link WebApplication#getRequestCycleListeners()},
+ * which will log {@link Logger#error(String)} and exception then show {@link FriendlyErrorPage}.
+ * @author ceefour
+ */
+public class FriendlyErrorRequestListener extends AbstractRequestCycleListener {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(FriendlyErrorRequestListener.class);
+	
+	/* (non-Javadoc)
+	 * @see org.apache.wicket.request.cycle.IRequestCycleListener#onException(org.apache.wicket.request.cycle.RequestCycle, java.lang.Exception)
+	 */
+	@Override
+	public IRequestHandler onException(RequestCycle cycle, Exception ex) {
+		final UUID incidentId = UUID.randomUUID();
+		log.error(MDC.get(CommandRequestAttributes.MDC_TENANT_ID) + " " + incidentId + " " + cycle.getRequest().getUrl() + "» " + ex, ex);
+		return new RenderPageRequestHandler(new PageProvider(new FriendlyErrorPage(incidentId, ex)), RedirectPolicy.NEVER_REDIRECT);
+	}
+
+}
