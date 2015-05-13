002fd49e073b37fd90de4625a63335745a27e14f
Hendy Irawan <ceefour666@gmail.com>
2015-05-13 15:45:02 +0700

    Revert to wicket-bootstrap 0.9.7
    * 0.10.0 requires wicket7
    * 0.9.8 has buggy Modal

--- bootstrap/src/main/java/org/soluvas/web/bootstrap/JQueryUiToTop.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/JQueryUiToTop.java
@@ -1,10 +1,7 @@
 package org.soluvas.web.bootstrap;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Iterables;
-import de.agilecoders.wicket.core.Bootstrap;
-import de.agilecoders.wicket.core.markup.html.references.JQueryPluginUrlResourceReference;
-import de.agilecoders.wicket.core.settings.IBootstrapSettings;
+import java.util.List;
+
 import org.apache.wicket.markup.head.CssHeaderItem;
 import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
@@ -13,6 +10,14 @@ import org.apache.wicket.request.resource.CssResourceReference;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.apache.wicket.request.resource.ResourceReference;
 import org.apache.wicket.request.resource.UrlResourceReference;
+
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.markup.html.references.JQueryPluginUrlResourceReference;
+import de.agilecoders.wicket.core.settings.IBootstrapSettings;
 
 /**
  * @author rio
@@ -31,16 +36,19 @@ public class JQueryUiToTop {
 			return Iterables.concat(super.getDependencies(), ImmutableList.<HeaderItem>of(
 					JavaScriptHeaderItem.forReference(JQueryEasing.instance()), 
 					CssHeaderItem.forReference(TOTOP_CSS)));
-		};
+		}
 	};
 	private static final UrlResourceReference TOTOP_CDN = new JQueryPluginUrlResourceReference(
 			Url.parse("//cdn.jsdelivr.net/jquery.uitotop/1.2/js/jquery.ui.totop.min.js")) {
+		@SuppressWarnings("unchecked")
 		@Override
-		public Iterable<? extends HeaderItem> getDependencies() {
-			return Iterables.concat(super.getDependencies(), ImmutableList.of(
-					JavaScriptHeaderItem.forReference(JQueryEasing.instance()),
-					CssHeaderItem.forUrl("//cdn.jsdelivr.net/jquery.uitotop/1.2/css/ui.totop.css")));
-		};
+		public List<HeaderItem> getDependencies() {
+			return FluentIterable.from((Iterable<HeaderItem>) super.getDependencies())
+					.append(
+						JavaScriptHeaderItem.forReference(JQueryEasing.instance()),
+						CssHeaderItem.forUrl("//cdn.jsdelivr.net/jquery.uitotop/1.2/css/ui.totop.css"))
+					.toList();
+		}
 	};
 	
 	/**
