5a4004bea7bf684132b5246b3b10697518828907
Rudi Wijaya <rudi_mail90@yahoo.com>
2015-06-19 04:52:40 -0430

    BC-3161 #comment sementera pake termListModel 2 yg di bippo

--- bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/TermCheckBoxMultipleChoice.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/TermCheckBoxMultipleChoice.java
@@ -2,11 +2,15 @@ package org.soluvas.web.bootstrap.widget;
 
 import java.util.List;
 
+import org.apache.wicket.injection.Injector;
 import org.apache.wicket.markup.html.form.CheckBoxMultipleChoice;
 import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.util.ListModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.soluvas.data.Term;
-import org.soluvas.web.site.TermListModel;
+import org.soluvas.data.TermRepository;
+import org.soluvas.data.domain.PageRequest;
 
 /**
  * @author rudi
@@ -15,6 +19,9 @@ import org.soluvas.web.site.TermListModel;
 public class TermCheckBoxMultipleChoice extends CheckBoxMultipleChoice<Term> {
 	
 	private static final long serialVersionUID = 1L;
+	
+	@SpringBean(name="shoeSizeTermRepo")
+	private TermRepository shoeSizeTermRepo;
 	
 	private class TermIChoiceRenderer implements IChoiceRenderer<Term> {
 
@@ -33,7 +40,12 @@ public class TermCheckBoxMultipleChoice extends CheckBoxMultipleChoice<Term> {
 
 	public TermCheckBoxMultipleChoice(final String id, final IModel<List<Term>> model,
 			final String kindNsPrefix, final String kindName) {
-		super(id, model, new TermListModel(kindNsPrefix, kindName));
+		//FIXME: harusnya pake TermListModel, tapi karena ada di soluvas, tidak bisa di pakai dlo karena bentrok ama versi 6.2
+//		super(id, model, new TermListModel(kindNsPrefix, kindName));
+		super(id);
+		Injector.get().inject(this);
+		setModel( (IModel) model);
+		setChoices(new ListModel<>(shoeSizeTermRepo.findAll(new PageRequest(0, 5000)).getContent()));
 		setChoiceRenderer(new TermIChoiceRenderer());
 		setPrefix("<span style='margin-right: 7px;'>");
 		setSuffix("</span>");
