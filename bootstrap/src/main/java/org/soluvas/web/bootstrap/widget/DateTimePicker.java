b46a1c32ab718b8c399420fcd4c748949a358f62
Hendy Irawan <ceefour666@gmail.com>
2016-04-18 20:18:31 +0700

    Can upload article image, yay! :)

---
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/widget/DateTimePicker.java
@@ -0,0 +1,42 @@
+package org.soluvas.web.bootstrap.widget;
+
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.form.datetime.DatetimePicker;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.form.datetime.DatetimePickerConfig;
+import org.apache.wicket.model.IModel;
+import org.joda.time.DateTime;
+import org.soluvas.web.site.DateTimeModel;
+
+import java.util.Date;
+
+/**
+ * Convenience component to use {@link DatetimePicker} with a {@link DateTime} model instead of {@link Date}.
+ * Created by ceefour on 18/04/2016.
+ */
+public class DateTimePicker extends DatetimePicker {
+    /**
+     * @param id
+     * @param datePattern See <a href="http://momentjs.com/docs/#/displaying/format/">momentjs' docs</a> for valid formats. Format also dictates what components are shown, e.g. MM/dd/YYYY will not display the time picker. Default is {@code MM/dd/yyyy}.
+     */
+    public DateTimePicker(String id, String datePattern) {
+        super(id, datePattern);
+    }
+
+    /**
+     * @param id
+     * @param datePattern See <a href="http://momentjs.com/docs/#/displaying/format/">momentjs' docs</a> for valid formats. Format also dictates what components are shown, e.g. MM/dd/YYYY will not display the time picker. Default is {@code MM/dd/yyyy}.
+     * @param id
+     * @param model
+     * @param datePattern
+     */
+    public DateTimePicker(String id, IModel<DateTime> model, String datePattern) {
+        super(id, new DateTimeModel(model), datePattern);
+    }
+
+    public DateTimePicker(String id, DatetimePickerConfig config) {
+        super(id, config);
+    }
+
+    public DateTimePicker(String id, IModel<DateTime> model, DatetimePickerConfig config) {
+        super(id, new DateTimeModel(model), config);
+    }
+}
---
+++ site/src/main/java/org/soluvas/web/site/DateTimeModel.java
@@ -0,0 +1,43 @@
+package org.soluvas.web.site;
+
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.joda.time.DateTime;
+
+import java.util.Date;
+
+/**
+ * Wraps a {@link DateTime} model as regular {@link Date} model,
+ * for use with {@code de.agilecoders.wicket.extensions.markup.html.bootstrap.form.datetime.DateTimePicker}.
+ * Created by ceefour on 18/04/2016.
+ */
+public class DateTimeModel implements IModel<Date> {
+
+    private IModel<DateTime> actualModel;
+
+    public DateTimeModel(IModel<DateTime> actualModel) {
+        this.actualModel = actualModel;
+    }
+
+    @Override
+    public Date getObject() {
+        return actualModel != null && actualModel.getObject() != null ?
+                actualModel.getObject().toDate() : null;
+    }
+
+    @Override
+    public void setObject(Date object) {
+        if (object != null && actualModel != null && actualModel.getObject() != null) {
+            actualModel.setObject(new DateTime(object).withZone(actualModel.getObject().getZone()));
+        } else if (object != null) {
+            actualModel.setObject(new DateTime(object));
+        } else {
+            actualModel.setObject(null);
+        }
+    }
+
+    @Override
+    public void detach() {
+        actualModel.detach();
+    }
+}
