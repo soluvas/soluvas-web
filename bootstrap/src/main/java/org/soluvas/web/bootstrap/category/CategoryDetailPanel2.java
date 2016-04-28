5b32a8cffd7161729935094b42a02807be54fb36
Rudi Wijaya <rudi_mail90@yahoo.com>
2016-04-28 09:42:42 -0430

    BC-3856 #support translations

--- bootstrap/src/main/java/org/soluvas/web/bootstrap/category/CategoryDetailPanel2.java
+++ bootstrap/src/main/java/org/soluvas/web/bootstrap/category/CategoryDetailPanel2.java
@@ -1,16 +1,19 @@
 package org.soluvas.web.bootstrap.category;
 
-import com.google.common.base.Objects;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Predicate;
-import com.google.common.base.Strings;
-import com.google.common.collect.Iterables;
-import com.mongodb.DuplicateKeyException;
-import de.agilecoders.wicket.core.markup.html.bootstrap.button.BootstrapAjaxButton;
-import de.agilecoders.wicket.core.markup.html.bootstrap.button.Buttons;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesomeIconType;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.ladda.LaddaAjaxButton;
-import de.agilecoders.wicket.extensions.markup.html.bootstrap.ladda.LaddaAjaxLink;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
 import org.apache.jena.ext.com.google.common.base.Optional;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
@@ -22,22 +25,35 @@ import org.apache.wicket.ajax.form.OnChangeAjaxBehavior;
 import org.apache.wicket.ajax.json.JSONObject;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.NumberTextField;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.GenericPanel;
-import org.apache.wicket.model.*;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.model.util.ListModel;
 import org.apache.wicket.model.util.MapModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.soluvas.category.*;
+import org.soluvas.category.Category;
+import org.soluvas.category.Category2;
+import org.soluvas.category.CategoryStatus;
+import org.soluvas.category.FormalCategory;
+import org.soluvas.category.FormalCategoryRepository;
+import org.soluvas.category.MongoCategoryRepository;
+import org.soluvas.category.MongoCategoryRepositoryImpl;
 import org.soluvas.commons.AppManifest;
 import org.soluvas.commons.SlugUtils;
 import org.soluvas.commons.tenant.TenantRef;
-import org.soluvas.data.Mixin;
 import org.soluvas.data.MixinManager;
 import org.soluvas.data.PropertyDefinition;
 import org.soluvas.web.site.OnChangeThrottledBehavior;
@@ -45,13 +61,18 @@ import org.soluvas.web.site.SeoBookmarkableMapper;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 
-import javax.annotation.Nullable;
-import javax.inject.Inject;
-import java.util.*;
-import java.util.Map.Entry;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.base.Strings;
+import com.google.common.collect.Iterables;
+import com.mongodb.DuplicateKeyException;
+
+import de.agilecoders.wicket.core.markup.html.bootstrap.button.BootstrapAjaxButton;
+import de.agilecoders.wicket.core.markup.html.bootstrap.button.Buttons;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesomeIconType;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.ladda.LaddaAjaxButton;
+import de.agilecoders.wicket.extensions.markup.html.bootstrap.ladda.LaddaAjaxLink;
 
 /**
  * View/edit a {@link Category}, only editable if nsPrefix != base.
@@ -207,15 +228,16 @@ public class CategoryDetailPanel2 extends GenericPanel<Category2> {
 	protected void onInitialize() {
 		super.onInitialize();
 		
+		form = new Form<>("form");
+		add(form);
+		
 		categoryLocaleModel.setObject(Locale.forLanguageTag(getModelObject().getLanguage()));
 		initLocalesAndTranslationMapModel();
 		
 		final boolean editable = !"base".equals(getModelObject().getNsPrefix());
 		add(new Label("kind", kindDisplayName));
-		add(new BookmarkablePageLink<>("backLink", backPage));
+		form.add(new BookmarkablePageLink<>("backLink", backPage));
 		
-		form = new Form<>("form");
-		add(form);
 		
 		form.add(new Label("parentUName", new AbstractReadOnlyModel<String>() {
 			@Override
@@ -478,7 +500,7 @@ public class CategoryDetailPanel2 extends GenericPanel<Category2> {
 			}
 		}.setIconType(FontAwesomeIconType.check);
 		saveBtn.setEnabled(editable);
-		add(saveBtn);
+		form.add(saveBtn);
 		
 		final BootstrapAjaxButton deleteBtn = new LaddaAjaxButton("deleteBtn", new Model<>("Delete"), Buttons.Type.Danger) {
 			@Override
@@ -503,7 +525,7 @@ public class CategoryDetailPanel2 extends GenericPanel<Category2> {
 		}.setIconType(FontAwesomeIconType.trash_o);
 		deleteBtn.setEnabled(editable);
 		deleteBtn.setVisible(false); // don't allow them to delete because old products might have inconsistent data. if want to delete, ask developers :)
-		add(deleteBtn);
+		form.add(deleteBtn);
 		
 		/*LANGUAGE BUTTONS*/
 		final WebMarkupContainer wmcLocales = new WebMarkupContainer("wmcLocales");
