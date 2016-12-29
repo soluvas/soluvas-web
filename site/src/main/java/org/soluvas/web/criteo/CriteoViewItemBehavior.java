f50af8d465ede23e0416f3000cd7603ef8650470
Atang Sutisna <atang@bippo.co.id>
2016-12-29 17:18:32 +0700

    BC-4190: add view item behavior

---
+++ site/src/main/java/org/soluvas/web/criteo/CriteoViewItemBehavior.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.soluvas.web.criteo;
+
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.StringHeaderItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
+
+/**
+ * @author atang
+ *
+ */
+@SuppressWarnings("serial")
+public class CriteoViewItemBehavior extends Behavior {
+	private static final Logger log = LoggerFactory
+			.getLogger(CriteoViewItemBehavior.class);
+	
+	private final String itemId;
+
+	public CriteoViewItemBehavior(String itemId) {
+		super();
+		this.itemId = itemId;
+	}
+	
+	@Override
+	public void renderHead(Component component, IHeaderResponse response) {
+		super.renderHead(component, response);
+		//TODO: ambil dari sysconfig
+		final boolean isCriteoEnable = true;
+		final String criteoPartnerId = "35754";
+		final String email = "atang@bippo.co.id";
+		
+		if (isCriteoEnable) {
+			if (Strings.isNullOrEmpty(criteoPartnerId)) {
+				log.warn("Criteo Partner ID must be set");
+			} else {
+				String criteoScript = "\n<script type=\"text/javascript\" src=\"//static.criteo.net/js/ld/ld.js\" async=\"true\"></script>\n";
+				String mainScript = String.format("\t\twindow.criteo_q = window.criteo_q || [];\n"
+						+ "\t\twindow.criteo_q.push(\n"
+						+ "\t\t{event: \"setAccount\", account: %s},\n"
+						+ "\t\t{event: \"setEmail\", email: \"%s\"},\n"
+						+ "\t\t{event: \"setSiteType\", type: \"d\"},\n"
+						+ "\t\t{event: \"viewItem\", item: \"%s\"}\n"
+						+ ");", criteoPartnerId, email, itemId);
+				criteoScript += "<script type=\"text/javascript\">\n"+ mainScript +"</script>";
+				response.render(StringHeaderItem.forString(criteoScript));
+			}
+		}
+	}
+}
