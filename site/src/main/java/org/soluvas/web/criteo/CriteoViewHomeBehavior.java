2c9e5c26b02ebd362001e576fadbfe8827057feb
Atang Sutisna <atang@bippo.co.id>
2017-01-02 11:36:02 +0700

    BC-4190: update view home criteo script

--- site/src/main/java/org/soluvas/web/criteo/CriteoViewHomeBehavior.java
+++ site/src/main/java/org/soluvas/web/criteo/CriteoViewHomeBehavior.java
@@ -11,6 +11,7 @@ import org.apache.wicket.markup.head.StringHeaderItem;
 import org.apache.wicket.model.IModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.soluvas.commons.PersonInfo;
 
 import com.google.common.base.Strings;
 
@@ -24,11 +25,11 @@ public class CriteoViewHomeBehavior extends Behavior {
 	private static final Logger log = LoggerFactory
 			.getLogger(CriteoViewHomeBehavior.class);
 	
-	private final IModel<String> emailModel;
+	private final IModel<PersonInfo> personInfoModel;
 	
-	public CriteoViewHomeBehavior(IModel<String> emailModel) {
+	public CriteoViewHomeBehavior(IModel<PersonInfo> personInfoModel) {
 		super();
-		this.emailModel = emailModel;
+		this.personInfoModel = personInfoModel;
 	}
 	
 	@Override
@@ -37,20 +38,30 @@ public class CriteoViewHomeBehavior extends Behavior {
 		//TODO: ambil dari sysconfig
 		final boolean isCriteoEnable = true;
 		final String criteoPartnerId = "35754";
-		final String emailHash = DigestUtils.md5Hex("atang@bippo.co.id");
 		
 		if (isCriteoEnable) {
 			if (Strings.isNullOrEmpty(criteoPartnerId)) {
 				log.warn("Criteo Partner ID must be set");
 			} else {
 				String criteoScript = "\n<script type=\"text/javascript\" src=\"//static.criteo.net/js/ld/ld.js\" async=\"true\"></script>\n";
-				String mainScript = String.format("\t\twindow.criteo_q = window.criteo_q || [];\n"
-						+ "\t\twindow.criteo_q.push(\n"
-						+ "\t\t{event: \"setAccount\", account: %s},\n"
-						+ "\t\t{event: \"setEmail\", email: \"%s\"},\n"
-						+ "\t\t{event: \"setSiteType\", type: \"d\"},\n"
-						+ "\t\t{event: \"viewHome\"}\n"
-						+ ");", criteoPartnerId, emailHash);
+				String mainScript;
+				if (personInfoModel.getObject() != null) {
+					mainScript = String.format("\t\twindow.criteo_q = window.criteo_q || [];\n"
+							+ "\t\twindow.criteo_q.push(\n"
+							+ "\t\t{event: \"setAccount\", account: %s},\n"
+							+ "\t\t{event: \"setSiteType\", type: \"d\"},\n"
+							+ "\t\t{event: \"viewHome\"}\n"
+							+ ");", criteoPartnerId);
+				} else {
+					mainScript = String.format("\t\twindow.criteo_q = window.criteo_q || [];\n"
+							+ "\t\twindow.criteo_q.push(\n"
+							+ "\t\t{event: \"setAccount\", account: %s},\n"
+							+ "\t\t{event: \"setEmail\", email: \"%s\"},\n"
+							+ "\t\t{event: \"setSiteType\", type: \"d\"},\n"
+							+ "\t\t{event: \"viewHome\"}\n"
+							+ ");", criteoPartnerId, DigestUtils.md5Hex(personInfoModel.getObject().getEmail()));
+				}
+				
 				criteoScript += "<script type=\"text/javascript\">\n"+ mainScript +"</script>\n";
 				response.render(StringHeaderItem.forString(criteoScript));
 			}
