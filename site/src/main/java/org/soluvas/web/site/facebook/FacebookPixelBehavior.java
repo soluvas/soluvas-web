47c0fd723cd76b25e293dae63e247c59e5fc9c29
Rudi Wijaya <rudi_mail90@yahoo.com>
2017-05-09 03:06:55 -0400

    BC-4285 #comment ready boss

--- site/src/main/java/org/soluvas/web/site/facebook/FacebookPixelBehavior.java
+++ site/src/main/java/org/soluvas/web/site/facebook/FacebookPixelBehavior.java
@@ -4,7 +4,9 @@ import org.apache.wicket.Component;
 import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.StringHeaderItem;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.ocpsoft.common.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.soluvas.socmed.FacebookSysConfig;
@@ -30,7 +32,14 @@ public class FacebookPixelBehavior extends Behavior {
 	
 	@SpringBean(required=false)
 	private FacebookSysConfig facebookSysConfig;
+
+	private final IModel<String> emailModel;
 	
+	public FacebookPixelBehavior(final IModel<String> emailModel) {
+		super();
+		this.emailModel = emailModel;
+	}
+
 	@Override
 	public void renderHead(Component component, IHeaderResponse response) {
 		super.renderHead(component, response);
@@ -38,22 +47,23 @@ public class FacebookPixelBehavior extends Behavior {
 			// Google Analytics is required for server timing
 			if (facebookSysConfig.getFacebookPixelId() != null) {
 				final String script =
-						"<script>(function() {\n"
-						+ "var _fbq = window._fbq || (window._fbq = []);\n"
-						+ "if (!_fbq.loaded) {\n"
-						+ "var fbds = document.createElement('script');\n"
-						+ "fbds.async = true;\n"
-						+ "fbds.src = '//connect.facebook.net/en_US/fbds.js';\n"
-						+ "var s = document.getElementsByTagName('script')[0];\n"
-						+ "s.parentNode.insertBefore(fbds, s);\n"
-						+ "_fbq.loaded = true;\n"
-						+ "}\n"
-						+ "_fbq.push(['addPixelId', '" + facebookSysConfig.getFacebookPixelId() + "']);\n"
-						+ "})();\n"
-						+ "window._fbq = window._fbq || [];\n"
-						+ "window._fbq.push(['track', 'PixelInitialized', {}]);\n"
-						+ "</script>\n"
-						+ "<noscript><img height=\"1\" width=\"1\" alt=\"\" style=\"display:none\" src=\"https://www.facebook.com/tr?id=" + facebookSysConfig.getFacebookPixelId() + "&amp;ev=PixelInitialized\" /></noscript>";
+						"<!-- Facebook Pixel Code --> \n"
+						+ "<script> \n"
+						+ "!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod? \n"
+						+ "n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n; \n"
+						+ "n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0; \n"
+						+ "t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, \n"
+						+ "document,'script','https://connect.facebook.net/en_US/fbevents.js'); \n"
+						+ (Strings.isNullOrEmpty(emailModel.getObject()) 
+								? "fbq('init', '" + facebookSysConfig.getFacebookPixelId() + "'); // Insert your pixel ID here. \n"
+										: "fbq('init', '752308308246944', { em: '" + emailModel.getObject() + "' }); \n")
+						+ "fbq('track', 'PageView'); \n"
+						+ "</script> \n"
+						+ "<noscript><img height=\"1\" width=\"1\" style=\"display:none\" \n"
+						+ "src=\"https://www.facebook.com/tr?id=752308308246944&ev=PageView&noscript=1\" \n"
+						+ "/></noscript> \n"
+						+ "<!-- DO NOT MODIFY --> \n"
+						+ "<!-- End Facebook Pixel Code --> \n";
 				// do not put in footer-container, so we use StringHeaderItem instead of JavaScriptHeaderItem
 				// to "fool" de.agilecoders.wicket.core.markup.html.RenderJavaScriptToFooterHeaderResponseDecorator
 				response.render(StringHeaderItem.forString(script));
