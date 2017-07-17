716bcabea6a4ab80acb010f7a5832e71e2631496
Hendy Irawan <ceefour666@gmail.com>
2017-07-17 22:56:24 +0700

    Proper XML namespace is now working using JAXB-JDK7 (without JAXB-RI nor MOXy) Thanks https://stackoverflow.com/a/7662275/122441

--- site/src/main/java/org/soluvas/web/site/opensearch/package-info.java
+++ site/src/main/java/org/soluvas/web/site/opensearch/package-info.java
@@ -5,13 +5,17 @@
 @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters(
         @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(type=DateTime.class, value=org.soluvas.commons.jaxb.JodaDateTimeAdapter.class)
 )
+// https://stackoverflow.com/a/7662275/122441
 @XmlSchema(
+        namespace = "http://a9.com/-/spec/opensearch/1.1/",
         xmlns={
                 @XmlNs(prefix="", namespaceURI="http://a9.com/-/spec/opensearch/1.1/"),
-                @XmlNs(prefix="moz", namespaceURI="http://www.mozilla.org/2006/browser/search/")})
+                @XmlNs(prefix="moz", namespaceURI="http://www.mozilla.org/2006/browser/search/")},
+        elementFormDefault = XmlNsForm.QUALIFIED)
 package org.soluvas.web.site.opensearch;
 
 import org.joda.time.DateTime;
 
 import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
--- site/src/main/java/org/soluvas/web/site/sitemap/package-info.java
+++ site/src/main/java/org/soluvas/web/site/sitemap/package-info.java
@@ -6,14 +6,18 @@
 @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters(
 	@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(type=DateTime.class, value=org.soluvas.commons.jaxb.JodaDateTimeAdapter.class)
 )
+// https://stackoverflow.com/a/7662275/122441
 @XmlSchema(
+	namespace = "http://www.sitemaps.org/schemas/sitemap/0.9",
 	xmlns={
 		@XmlNs(prefix="", namespaceURI="http://www.sitemaps.org/schemas/sitemap/0.9"),
 		@XmlNs(prefix="image", namespaceURI="http://www.google.com/schemas/sitemap-image/1.1"),
-		@XmlNs(prefix="video", namespaceURI="http://www.google.com/schemas/sitemap-video/1.1")})
+		@XmlNs(prefix="video", namespaceURI="http://www.google.com/schemas/sitemap-video/1.1")},
+	elementFormDefault = XmlNsForm.QUALIFIED)
 package org.soluvas.web.site.sitemap;
 
 import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
 
 import org.joda.time.DateTime;
