d0ee7d6c0949421750dc85902708e4a6ba836786
Hendy Irawan <ceefour666@gmail.com>
2017-07-31 14:34:20 +0800

    Disable Google Analytics if ROLE_ADMIN authority.

--- site/src/main/java/org/soluvas/web/site/google/GoogleAnalyticsBehavior.java
+++ site/src/main/java/org/soluvas/web/site/google/GoogleAnalyticsBehavior.java
@@ -14,6 +14,8 @@ import org.apache.wicket.request.Url;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.soluvas.web.site.IGoogleAnalyticsSysConfig;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.annotation.Nullable;
 import javax.inject.Inject;
@@ -25,6 +27,8 @@ import java.util.List;
  * <p>Google Analytics tracking <a href="https://idbippo.atlassian.net/browse/BC-1397">was not enabled on app/mall role</a>.
  * However, Google Analytics is useful for other things, including server response time tracking,
  * so it should be enabled on all pages including backend pages.
+ *
+ * <p>It disables itself if a {@code ROLE_ADMIN} user is logged in (supported in Spring Security only).</p>
  * 
  * @author ceefour
  */
@@ -37,6 +41,21 @@ public class GoogleAnalyticsBehavior extends Behavior {
 	@Inject
 	private IGoogleAnalyticsSysConfig sysConfig;
 
+	public static boolean isAdmin() {
+		try {
+			final Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			return auth.getAuthorities().stream().anyMatch(it -> "ROLE_ADMIN".equals(it.getAuthority()));
+		} catch (Exception e) {
+			log.trace("Spring Security not used", e);
+		}
+		return false;
+	}
+
+	public static boolean isReallyEnabled(IGoogleAnalyticsSysConfig sysConfig) {
+		return null != sysConfig && Boolean.TRUE == sysConfig.getGoogleAnalyticsEnabled() &&
+				!Strings.isNullOrEmpty(sysConfig.getGoogleAnalyticsTrackingId()) && !isAdmin();
+	}
+
 	@Override
 	public void renderHead(Component component, IHeaderResponse response) {
 		super.renderHead(component, response);
@@ -45,37 +64,39 @@ public class GoogleAnalyticsBehavior extends Behavior {
 			if (Strings.isNullOrEmpty(sysConfig.getGoogleAnalyticsTrackingId())) {
 				log.warn("Google Analytics Tracking ID must be set");
 			} else {
-				// TODO: support specific domain (i.e. non-auto) during ga(create)
-				// https://developers.google.com/analytics/devguides/collection/analyticsjs/
-				final String realCookieDomain = Optional.fromNullable(sysConfig.getGoogleAnalyticsCookieDomain()).or("auto");
-				String googleAnalyticScript =
-						  "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n"
-						+ "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n"
-						+ "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n"
-						+ "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n"
-						+ "\n"
-						+ "ga('create', '" + sysConfig.getGoogleAnalyticsTrackingId() + "', '" + realCookieDomain + "');\n";
-				if (Boolean.TRUE == sysConfig.getGoogleAnalyticsDisplayFeatures()) {
-					googleAnalyticScript += "ga('require', 'displayfeatures');\n";
+				if (!isAdmin()) {
+					// TODO: support specific domain (i.e. non-auto) during ga(create)
+					// https://developers.google.com/analytics/devguides/collection/analyticsjs/
+					final String realCookieDomain = Optional.fromNullable(sysConfig.getGoogleAnalyticsCookieDomain()).or("auto");
+					String googleAnalyticScript =
+							"(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n"
+									+ "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n"
+									+ "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n"
+									+ "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n"
+									+ "\n"
+									+ "ga('create', '" + sysConfig.getGoogleAnalyticsTrackingId() + "', '" + realCookieDomain + "');\n";
+					if (Boolean.TRUE == sysConfig.getGoogleAnalyticsDisplayFeatures()) {
+						googleAnalyticScript += "ga('require', 'displayfeatures');\n";
+					}
+					// result e.g. "id/search?4&religion=BUDDHISM"
+					final Url baseUrl = component.getRequestCycle().getUrlRenderer().getBaseUrl();
+					final String fixedUrl;
+					if (baseUrl.toString().matches(".*\\?\\d+=?")) {
+						fixedUrl = "/" + baseUrl.toString().replaceFirst("\\?\\d+=?", "");
+					} else if (baseUrl.toString().matches(".*\\?\\d+=?&.*")) {
+						fixedUrl = "/" + baseUrl.toString().replaceFirst("\\?\\d+=?&", "?");
+					} else {
+						fixedUrl = "/" + baseUrl.toString();
+					}
+					log.trace("Current URL: base={}, fixeds={}", baseUrl, fixedUrl);
+					// Sets the page value on the tracker.
+					googleAnalyticScript += "ga('set', 'page', " + JSONObject.quote(fixedUrl) + ");\n";
+					// https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#hit
+					googleAnalyticScript += "ga('send', 'pageview');\n";
+					// do not put in footer-container, so we use StringHeaderItem instead of JavaScriptHeaderItem
+					// to "fool" de.agilecoders.wicket.core.markup.html.RenderJavaScriptToFooterHeaderResponseDecorator
+					response.render(StringHeaderItem.forString("<script>\n" + googleAnalyticScript + "</script>\n"));
 				}
-				// result e.g. "id/search?4&religion=BUDDHISM"
-				final Url baseUrl = component.getRequestCycle().getUrlRenderer().getBaseUrl();
-				final String fixedUrl;
-				if (baseUrl.toString().matches(".*\\?\\d+=?")) {
-					fixedUrl = "/" + baseUrl.toString().replaceFirst("\\?\\d+=?", "");
-				} else if (baseUrl.toString().matches(".*\\?\\d+=?&.*")) {
-					fixedUrl = "/" + baseUrl.toString().replaceFirst("\\?\\d+=?&", "?");
-				} else {
-					fixedUrl = "/" + baseUrl.toString();
-				}
-				log.trace("Current URL: base={}, fixeds={}", baseUrl, fixedUrl);
-				// Sets the page value on the tracker.
-				googleAnalyticScript += "ga('set', 'page', " + JSONObject.quote(fixedUrl) + ");\n";
-				// https://developers.google.com/analytics/devguides/collection/analyticsjs/field-reference#hit
-				googleAnalyticScript += "ga('send', 'pageview');\n";
-				// do not put in footer-container, so we use StringHeaderItem instead of JavaScriptHeaderItem
-				// to "fool" de.agilecoders.wicket.core.markup.html.RenderJavaScriptToFooterHeaderResponseDecorator
-				response.render(StringHeaderItem.forString("<script>\n" + googleAnalyticScript + "</script>\n"));
 			}
 		}
 	}
@@ -98,7 +119,7 @@ public class GoogleAnalyticsBehavior extends Behavior {
 		final String consoleLog = String.format("ga.send.event category=%s action=%s label=%s value=%s",
 			category, action, label, value);
 		target.appendJavaScript("console.debug(" + JSONObject.quote(consoleLog) + ");\n");
-		if (null != config && Boolean.TRUE == config.getGoogleAnalyticsEnabled() && !Strings.isNullOrEmpty(config.getGoogleAnalyticsTrackingId())) {
+		if (isReallyEnabled(config)) {
 			target.appendJavaScript("ga('send', 'event', " + JSONObject.quote(category) + ", " + JSONObject.quote(action )+ ", " +
 					JSONObject.quote(label) + ", " + (null != value ? value : "null") + ");\n");
 		}
@@ -119,7 +140,7 @@ public class GoogleAnalyticsBehavior extends Behavior {
 		final String consoleLog = String.format("ga.send.event[beacon] category=%s action=%s label=%s value=%s",
 				category, action, label, value);
 		script += "console.debug(" + JSONObject.quote(consoleLog) + "); ";
-		if (null != config && Boolean.TRUE == config.getGoogleAnalyticsEnabled() && !Strings.isNullOrEmpty(config.getGoogleAnalyticsTrackingId())) {
+		if (isReallyEnabled(config)) {
 			script += " ga('send', 'event', " + JSONObject.quote(category) + ", " + JSONObject.quote(action) + ", " +
 					JSONObject.quote(label) + ", " + (null != value ? value : "null") + ", {transport: 'beacon'});";
 		}
@@ -141,7 +162,7 @@ public class GoogleAnalyticsBehavior extends Behavior {
 		final String consoleLog = String.format("ga.send.event[nonInteraction] category=%s action=%s label=%s value=%s",
 				category, action, label, value);
 		script += "console.debug(" + JSONObject.quote(consoleLog) + "); ";
-		if (null != config && Boolean.TRUE == config.getGoogleAnalyticsEnabled() && !Strings.isNullOrEmpty(config.getGoogleAnalyticsTrackingId())) {
+		if (isReallyEnabled(config)) {
 			script += " ga('send', 'event', " + JSONObject.quote(category) + ", " + JSONObject.quote(action) + ", " +
 					JSONObject.quote(label) + ", " + (null != value ? value : "null") + ", {nonInteraction: true});";
 		}
@@ -157,11 +178,13 @@ public class GoogleAnalyticsBehavior extends Behavior {
 	 */
 	public static void reportErrors(IGoogleAnalyticsSysConfig config, AjaxRequestTarget target,
 									Component component) {
-		final List<FeedbackMessage> messages = new FeedbackCollector(component).collect();
-		messages.forEach(msg ->
-				GoogleAnalyticsBehavior.appendSendEvent(config, target,
-						component.getPage().getPageClass().getSimpleName(), "error " + msg.getReporter().getId(),
-						msg.toString(), msg.getLevel()));
+		if (isReallyEnabled(config)) {
+			final List<FeedbackMessage> messages = new FeedbackCollector(component).collect();
+			messages.forEach(msg ->
+					GoogleAnalyticsBehavior.appendSendEvent(config, target,
+							component.getPage().getPageClass().getSimpleName(), "error " + msg.getReporter().getId(),
+							msg.toString(), msg.getLevel()));
+		}
 	}
 
 }
